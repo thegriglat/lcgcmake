af3f908180c5af8c4a05cc27782f2b902b92db8f
mato <mato@4525493e-7705-40b1-a816-d608a930855b>
2013-02-01 16:04:11 +0000

    1) Added extra step to install sources in /share/source; 2) Added global target for multi-version packages to build (clean) all versions; 3) Added protection on DEPENDS parameter
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@4400 4525493e-7705-40b1-a816-d608a930855b

--- cmake/modules/lcgsoft-macros.cmake
+++ cmake/modules/lcgsoft-macros.cmake
@@ -13,11 +13,21 @@ macro(LCGPackage_Add name)
 
   CMAKE_PARSE_ARGUMENTS(ARG "" "" "DEPENDS" ${ARGN})   # This special handling is needed until CMake 2.8.11 is released
   
-  set(targetname ${name})
-  set(versionindex 1)
+  #---Check if this is a muli-version package or not-------------------------------------------------
+  list(LENGTH ${name}_native_version nvers)
+  if(nvers GREATER 1)
+    add_custom_target(${name} ALL COMMENT "Multi-version package ${name} global target")
+    add_custom_target(clean-${name} COMMENT "Clean a multi-version package ${name}")
+  endif()
   
   #---Loop over all versions of the package----------------------------------------------------------
   foreach( version ${${name}_native_version} )
+  
+    if(nvers GREATER 1)
+      set(targetname ${name}-${version})
+    else()
+      set(targetname ${name})
+    endif()
 
     #---Check if the package is already existing in the installation area
     if(EXISTS ${CMAKE_INSTALL_PREFIX}/${${name}_directory_name}/${version}/${LCG_system})
@@ -32,6 +42,8 @@ macro(LCGPackage_Add name)
 
       #---Replace patterns for multiversion cases-----------------------------------------------------
       string(REPLACE <NATIVE_VERSION> ${version} ARGUMENTS "${ARG_UNPARSED_ARGUMENTS}")
+      
+      #---Add the external project -------------------------------------------------------------------
 
       ExternalProject_Add(
         ${targetname}
@@ -40,10 +52,24 @@ macro(LCGPackage_Add name)
         ${ARGUMENTS}
         LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1 )
 
+      #---Adding extra step to copy the sources in /share/sources-------------------------------------
+      ExternalProject_Add_Step(${targetname} install_sources COMMENT "Installing sources for ${targetname}"
+        COMMAND  ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR> ${LOCAL_INSTALL_PREFIX}/${${name}_directory_name}/${version}/share/sources
+        DEPENDERS build
+        DEPENDEES update)
+        
+
+      #---Adding clean targets--------------------------------------------------------------------------
       add_custom_target(clean-${targetname} COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_CURRENT_BINARY_DIR}/${targetname}
                                             COMMAND ${CMAKE_COMMAND} -E remove_directory ${${name}_home})
 
+      #---Handle dependencies-----(should not be needed after CMake 2.8.11)-----------------------------
       if(ARG_DEPENDS)
+         foreach(dep ${ARG_DEPENDS})
+           if(NOT TARGET ${dep})
+             message(SEND_ERROR "Package ${name} declared a dependency to non existing package ${dep}")
+           endif()
+         endforeach()
          add_dependencies(${targetname} ${ARG_DEPENDS})
       endif()
 
@@ -56,10 +82,12 @@ macro(LCGPackage_Add name)
       endforeach()
 
     endif()
-
-    set(targetname ${name}_${versionindex})
-    math(EXPR versionindex ${versionindex}+1)
     
+    if(nvers GREATER 1)
+      add_dependencies(${name} ${targetname})
+      add_dependencies(clean-${name} clean-${targetname})
+    endif()
+
   endforeach()
 
   #--- export the latest ${name}_home variable to the other subdirectories
--- cmake/toolchain/heptools-64b.cmake
+++ cmake/toolchain/heptools-64b.cmake
@@ -163,8 +163,10 @@ LCG_external_package(xrootd            3.1.0p2                                  
 
 #---Additional External packages------(Generators)-----------------
 LCG_external_package(pythia8           "165;170"       MCGenerators/pythia8     )
+LCG_external_package(lhapdf            5.8.8           MCGenerators/lhapdf      )
 LCG_external_package(thepeg            1.8.1           MCGenerators/thepeg      )
 LCG_external_package(herwig++          2.6.1b          MCGenerators/herwig++    )
+LCG_external_package(tauola++          1.1.1a          MCGenerators/tauola++    )
 
 
 # Prepare the search paths according to the versions above
