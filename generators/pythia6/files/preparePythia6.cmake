81a1840dc011b5707fd461facb2e663777ec5fe7
mato <mato@4525493e-7705-40b1-a816-d608a930855b>
2013-02-15 17:35:33 +0000

    Deleted some of the obsolete autotools files. Fixed current directory
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@4500 4525493e-7705-40b1-a816-d608a930855b

--- generators/pythia6/files/preparePythia6.cmake
+++ generators/pythia6/files/preparePythia6.cmake
@@ -2,16 +2,15 @@ find_program(PERL_COMMAND perl)
 
 #---Split the original source file--------------------------------------------
 file(GLOB pythiafile  pythia-6.*.f)
-execute_process(COMMAND ${PERL_COMMAND} ${CMAKE_SOURCE_DIR}/../pythia6/splitter.pl ${pythiafile}
-                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/../pythia6/)
-                
+execute_process(COMMAND ${PERL_COMMAND} splitter.pl ${pythiafile})
+
 set(dummy_files fhhiggscorr.f fhsetflags.f fhsetpara.f pyevwt.f pykcut.f pytaud.f pytime.f ssmssm.f sugra.f upevnt.f upinit.f upveto.f visaje.f)
-file(COPY ../pythia6/${dummy_files} DESTINATION ${PROJECT_SOURCE_DIR}/../pythia6/dummy)
-file(REMOVE ../pythia6/${dummy_files})
+file(COPY ${dummy_files} DESTINATION dummy)
+file(REMOVE ${dummy_files})
 
 set(pdfdummy_files pdfset.f structm.f structp.f )
-file(COPY ../pythia6/${pdfdummy_files} DESTINATION ${PROJECT_SOURCE_DIR}../pythia6//pdfdummy)
-file(REMOVE ../pythia6/${pdfdummy_files})
+file(COPY ${pdfdummy_files} DESTINATION pdfdummy)
+file(REMOVE ${pdfdummy_files})
 
 file(RENAME  dummy/upveto.f dummy/upveto.F)
 file(RENAME  pyhepc.f pyhepc.F)
