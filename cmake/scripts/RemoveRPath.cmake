75986a3dace1c57422d5d4a038089555d9539fc6
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2013-04-23 19:14:52 +0000

    handle rw permissions when stripping the rpath
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@4711 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/RemoveRPath.cmake
+++ cmake/scripts/RemoveRPath.cmake
@@ -4,6 +4,14 @@
 
 file(GLOB_RECURSE sharedlibs ${INSTALL_DIR}/*.so)
 foreach(file ${sharedlibs})
-  file(RPATH_REMOVE FILE "${file}" )
+  if(APPLE)
+    execute_process(COMMAND stat -f "%OLp" "${file}" OUTPUT_VARIABLE permissions)
+  else()
+    execute_process(COMMAND stat -c "%a" "${file}" OUTPUT_VARIABLE permissions)
+  endif()
+  string(REGEX REPLACE "(\r?\n)+$" "" permissions "${permissions}")
+  execute_process(COMMAND chmod ugo+wx "${file}")
+  file(RPATH_REMOVE FILE "${file}")
+  execute_process(COMMAND chmod ${permissions} "${file}")
 endforeach()
 
