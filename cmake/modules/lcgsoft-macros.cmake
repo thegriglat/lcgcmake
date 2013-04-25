9164f1299a0a3e3ea4ad83b35d89a28da4d5533b
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2013-04-25 12:10:06 +0000

    add functionality to retrieve the package dependencies and to dump them to file
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@4719 4525493e-7705-40b1-a816-d608a930855b

--- cmake/modules/lcgsoft-macros.cmake
+++ cmake/modules/lcgsoft-macros.cmake
@@ -15,6 +15,8 @@ include(CMakeParseArguments)
 #        - denoted with the switch "BUNDLE_PACKAGE"
 #     o supports pure binary package installations
 #        - denoted with the switch "BINARY_PACKAGE"
+#     o For every target it sets ${name}_home, ${name}_exists and ${name}_dependencies 
+#     o It uses ${name}_native_version which has to be set externally     
 #
 #----------------------------------------------------------------------------------------------------
 macro(LCGPackage_Add name)
@@ -31,7 +33,7 @@ macro(LCGPackage_Add name)
   
   #---Loop over all versions of the package----------------------------------------------------------
   foreach( version ${${name}_native_version} )
-  
+
     if(nvers GREATER 1)
       set(targetname ${name}-${version})
     else()
@@ -86,7 +88,7 @@ macro(LCGPackage_Add name)
 
       #---We cannot use the argument INSTALL_DIR becuase cmake itself will create the directory 
       #   unconditionaly before make is executed. So, we replace <INSTALL_DIR> before passing the
-      #   arguments to ExternalProeject_Add().
+      #   arguments to ExternalPreject_Add().
       string(REPLACE <INSTALL_DIR> ${${dest_name}_home} ARGUMENTS "${ARGUMENTS}")
 
       #---Add the external project -------------------------------------------------------------------
@@ -117,7 +119,6 @@ macro(LCGPackage_Add name)
                   ${CMAKE_COMMAND} -E tar cfz ${CMAKE_INSTALL_PREFIX}/${${name}_directory_name}/../distribution/${name}/${name}-${version}-${LCG_system}.tgz ${n_name}/${version}/${LCG_system}
           DEPENDEES strip_rpath)
       endif()
-
       #---Adding extra step to copy the log files----------------------------------------------------
       ExternalProject_Add_Step(${targetname} install_logs COMMENT "Installing log files for '${targetname}'"
           COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=${${dest_name}_home}/logs -DLOGS_DIR=${CMAKE_CURRENT_BINARY_DIR}/${targetname}/src/${targetname}-stamp  
@@ -163,26 +164,75 @@ macro(LCGPackage_Add name)
                                             COMMAND ${CMAKE_COMMAND} -E remove_directory ${${name}_home})
 
       #---Handle dependencies-----(should not be needed after CMake 2.8.11)-----------------------------
+      set(${targetname}_dependencies "")
       if(ARG_DEPENDS)
          foreach(dep ${ARG_DEPENDS})
            if(NOT TARGET ${dep})
              message(SEND_ERROR "Package ${name} declared a dependency to non existing package ${dep}")
            endif()
+            list(APPEND ${targetname}_dependencies ${dep})
          endforeach()
          add_dependencies(${targetname} ${ARG_DEPENDS})
       endif()
-
     endif()
     
     if(nvers GREATER 1)
       add_dependencies(${name} ${targetname})
       add_dependencies(clean-${name} clean-${targetname})
     endif()
-
   endforeach()
 
   #--- export the latest ${name}_home variable to the other subdirectories
   set(${name}_home ${${name}_home} PARENT_SCOPE)
 
+  #--- mark that the target exists
+  set(${name}_exists 1 PARENT_SCOPE)
+
+  #--- export the dependencies to the outside files 
+  set(${targetname}_dependencies ${${targetname}_dependencies} PARENT_SCOPE)
+
 endmacro()
 
+#----------------------------------------------------------------------------------------------------
+# LCGPackage_add_dependencies macro
+#----------------------------------------------------------------------------------------------------
+macro(LCGPackage_add_dependencies name )
+  SET(${name}_dependencies ${${name}_dependencies} ${ARGN})
+  add_dependencies(${name} ${ARGN})
+  set(${name}_dependencies ${${name}_dependencies} PARENT_SCOPE)
+endmacro()
+
+
+#----------------------------------------------------------------------------------------------------
+# LCGPackage_create_dependency_file function 
+#  o creates json and dot files containing the dependency tree
+#  o uses ${name}_dependencies, ${name}_exists and ${targetname}_native_version
+#----------------------------------------------------------------------------------------------------
+function(LCG_create_dependency_files)
+  set(dotfile ${CMAKE_BINARY_DIR}/dependencies.dot)
+  set(jsonfile ${CMAKE_BINARY_DIR}/dependencies.json)
+  file(WRITE ${dotfile} "digraph lcgcmake {\n")
+  file(WRITE ${jsonfile} "{\n")
+  foreach(targetname  ${LCG_externals} ${LCG_projects})
+    if(NOT ${targetname}_exists)
+      MESSAGE("WARNING: ${targetname} has a version (${targetname}_native_version), but no target defined.")
+    endif()
+    # clean up things dot doesn't understand
+    # eventually replace with REGEX REPLACE
+    string(REPLACE "+" "p" cleaned_name ${targetname})
+    string(REPLACE "-" "_" cleaned_name ${cleaned_name})
+    string(REPLACE "-" "_" cleaned_name ${cleaned_name})
+    file(APPEND ${dotfile} "_${cleaned_name}_ [label=\"${targetname}\"];\n")
+    set(json_string "'${targetname}' : {'version': '${${targetname}_native_version}', 'dependencies' : [")
+    foreach(dep ${${targetname}_dependencies})
+      string(REPLACE "+" "p" cleaned_dep ${dep})
+      set(json_string "${json_string} '${dep}',")
+      file(APPEND ${dotfile} "_${cleaned_name}_ -> _${cleaned_dep}_;\n")
+    endforeach()
+    set(json_string "${json_string} ]}")
+    file(APPEND ${jsonfile} ${json_string}\n)
+  endforeach()
+  file(APPEND ${dotfile} "}\n")
+  file(APPEND ${jsonfile} "}\n")
+  message("Wrote package dependency information to ${dotfile} and ${jsonfile}.")
+endfunction()
