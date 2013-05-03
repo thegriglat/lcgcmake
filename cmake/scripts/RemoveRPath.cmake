38ef6585c3aec1cb7a0355ce063a2a90af2421a4
mato <mato@4525493e-7705-40b1-a816-d608a930855b>
2013-05-03 17:13:34 +0000

    Attempt to fix a couple of warnings
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@4777 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/RemoveRPath.cmake
+++ cmake/scripts/RemoveRPath.cmake
@@ -2,6 +2,8 @@
 # Parameters:
 #    INSTALL_DIR Installation prefix
 
+cmake_policy(SET CMP0009 NEW) # FILE GLOB_RECURSE calls should not follow symlinks by default
+
 file(GLOB_RECURSE sharedlibs ${INSTALL_DIR}/*.so)
 foreach(file ${sharedlibs})
   if(NOT APPLE)
