1b4a21fc1822e4ff27ab57fd8a89854bd2570abc
dkonst <dkonst@4525493e-7705-40b1-a816-d608a930855b>
2013-05-14 21:02:59 +0000

    pyquen added to toolchain list
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@4828 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-preview.cmake
+++ cmake/toolchain/heptools-preview.cmake
@@ -174,6 +174,7 @@ LCG_external_package(tauola              28.121.2     ${MCGENPATH}/tauola       
 LCG_external_package(jimmy              4.31.3        ${MCGENPATH}/jimmy         )
 LCG_external_package(hydjet++           2_1           ${MCGENPATH}/hydjet++     )
 LCG_external_package(alpgen            2.1.4          ${MCGENPATH}/alpgen author=214 )
+LCG_external_package(pyquen           1.5.1           ${MCGENPATH}/pyquen     )
 
 # Prepare the search paths according to the versions above
 LCG_prepare_paths()
