fba2f75104e5e9ce544d29b4542b49650bc7b145
klugovsk <klugovsk@4525493e-7705-40b1-a816-d608a930855b>
2013-05-15 11:44:40 +0000

    empty ctests for generators
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@4840 4525493e-7705-40b1-a816-d608a930855b

--- generators/hydjet/test/runtest.sh
+++ generators/hydjet/test/runtest.sh
@@ -5,9 +5,10 @@ if [ "$1" != "" ]; then
  cd $1
 fi
 
+
 source /afs/.cern.ch/sw/lcg/external/MCGenerators/.work/GBUILD/noarch/TOOLS/genser.rc
 export MCGTESTDIR=$2
-./configure --with-hydjetversion=$3 --run-hydjet=mcgtest
+./configure --with-hydjetversion=1.8 --run-hydjet=mcg
 source config.mk
 make -f Makefile.hydjet hydjet_test1 PROGNAME=hydjet_test3
 source ldlp.sh
