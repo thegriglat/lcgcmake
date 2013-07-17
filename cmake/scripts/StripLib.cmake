7461cb88b7d79c21cac974cd50af4175d07e0fc3
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2013-07-17 00:16:32 +0000

    Add cmake script to strip debug symbols
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5053 4525493e-7705-40b1-a816-d608a930855b

---
+++ cmake/scripts/StripLib.cmake
@@ -0,0 +1,16 @@
+# Strip the debug symbols from all .so files in the given dir
+# Parameters:
+#    INSTALL_DIR Installation prefix
+
+cmake_policy(SET CMP0009 NEW) # FILE GLOB_RECURSE calls should not follow symlinks by default
+
+file(GLOB_RECURSE sharedlibs ${INSTALL_DIR}/*.so)
+foreach(file ${sharedlibs})
+  if(NOT APPLE)
+    execute_process(COMMAND stat -c "%a" "${file}" OUTPUT_VARIABLE permissions)
+    string(REGEX REPLACE "(\r?\n)+$" "" permissions "${permissions}")
+    execute_process(COMMAND chmod ugo+wx "${file}")
+    execute_process(COMMAND strip "${file}")
+    execute_process(COMMAND chmod ${permissions} "${file}")
+  endif()
+endforeach()
