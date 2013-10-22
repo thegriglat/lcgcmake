9e8acf6789be3e396fc35b97bc0a75e4ac2e1ab2
dkonst <dkonst@4525493e-7705-40b1-a816-d608a930855b>
2013-10-22 08:36:10 +0000

    version of sherpa2 updated to 2.0.0 for preview configuration
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5388 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-preview.cmake
+++ cmake/toolchain/heptools-preview.cmake
@@ -193,7 +193,7 @@ LCG_external_package(sherpa            1.4.3          ${MCGENPATH}/sherpa       
 LCG_external_package(sherpa            1.4.3.2        ${MCGENPATH}/sherpa         author=1.4.3 hepevt=10000 )
 LCG_external_package(sherpa            1.4.5          ${MCGENPATH}/sherpa         author=1.4.5 hepevt=4000 )
 LCG_external_package(sherpa            1.4.5.2        ${MCGENPATH}/sherpa         author=1.4.5 hepevt=10000 )
-LCG_external_package(sherpa            2.0.beta2      ${MCGENPATH}/sherpa         author=2.0.beta2 hepevt=10000)
+LCG_external_package(sherpa            2.0.0          ${MCGENPATH}/sherpa         author=2.0.0 hepevt=10000)
 
 LCG_external_package(hepmcanalysis     3.4.14         ${MCGENPATH}/hepmcanalysis  author=00-03-04-14        )
 LCG_external_package(mctester          1.25.0         ${MCGENPATH}/mctester     )
