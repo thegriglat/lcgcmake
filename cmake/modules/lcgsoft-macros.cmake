b7e1513dc13e32b48190312827723ff3e65db48c
dkonst <dkonst@4525493e-7705-40b1-a816-d608a930855b>
2013-10-28 15:59:21 +0000

    using lockfile to avoid simultanious writing to share directory
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5413 4525493e-7705-40b1-a816-d608a930855b

--- cmake/modules/lcgsoft-macros.cmake
+++ cmake/modules/lcgsoft-macros.cmake
@@ -166,7 +166,9 @@ macro(LCGPackage_Add name)
       if(NOT ARG_BINARY_PACKAGE) 
         ExternalProject_Add_Step(${targetname} install_sources
           COMMENT "Installing sources for '${targetname}' and create source tarball"
+	  COMMAND lockfile ${CMAKE_INSTALL_PREFIX}/${${dest_name}_directory_name}/${dest_version}/lock.txt
           COMMAND ${CMAKE_COMMAND} -DSRC=<SOURCE_DIR> -DDST=${CMAKE_INSTALL_PREFIX}/${${dest_name}_directory_name}/${dest_version}/share/sources/${curr_name} -P ${CMAKE_SOURCE_DIR}/cmake/scripts/copy.cmake
+	  COMMAND ${CMAKE_COMMAND} -E remove -f ${CMAKE_INSTALL_PREFIX}/${${dest_name}_directory_name}/${dest_version}/lock.txt
           COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/${${dest_name}_directory_name}/../distribution/${name}
           COMMAND ${CMAKE_COMMAND} -E chdir <SOURCE_DIR>/../.. ${CMAKE_COMMAND} -E tar cfz ${CMAKE_INSTALL_PREFIX}/${${dest_name}_directory_name}/../distribution/${name}/${name}-${version}-src.tgz ${name}
           DEPENDERS configure
