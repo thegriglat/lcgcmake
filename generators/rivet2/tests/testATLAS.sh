b3105835f0c21c0d9761f1eb0b8843882f008f23
dkonst <dkonst@4525493e-7705-40b1-a816-d608a930855b>
2013-10-28 11:18:31 +0000

    rivet2 tests updated
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5411 4525493e-7705-40b1-a816-d608a930855b

--- generators/rivet2/tests/testATLAS.sh
+++ generators/rivet2/tests/testATLAS.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 source $1/rivetenv.sh
-rivet --analysis=ATLAS_2012_I1083318 $2:
+rivet -a ATLAS_2012_I1083318 $2
+
+
--- generators/rivet2/tests/testCMS.sh
+++ generators/rivet2/tests/testCMS.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 source $1/rivetenv.sh
-
-echo "LD_LIBARARY_PATH =  " ${LD_LIBRARY_PATH}
-echo "PATH =  " ${PATH}
-rivet --analysis=CMS_2012_I1184941 $2:
+rivet -a CMS_2012_I1184941 $2
