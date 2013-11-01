832308258149f61d28916ae75320d96189529b92
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2013-11-01 20:07:23 +0000

    physical test
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5436 4525493e-7705-40b1-a816-d608a930855b

---
+++ generators/pythia8/tests/analyserhepmc/AnalyserHepMC.cc
@@ -0,0 +1,103 @@
+#include "AnalyserHepMC.h"
+
+struct ParticlePtGreater {
+  double operator () (const HepMC::GenParticle *v1,  
+                                  const HepMC::GenParticle *v2)
+  { return v1->momentum().perp() > v2->momentum().perp(); }
+};
+
+
+AnalyserHepMC::AnalyserHepMC()
+{;}
+
+
+void AnalyserHepMC::initialize()
+{
+  double Rparam = 0.5;
+  strategy = fastjet::Best;
+  recombScheme = fastjet::E_scheme;
+  jetDef = new fastjet::JetDefinition(fastjet::antikt_algorithm, Rparam,
+                                      recombScheme, strategy);
+
+  for (int ind=0; ind < 6; ind++) {icategories[ind]=0;}
+ 
+  return ;
+}
+
+
+void AnalyserHepMC::analyse(const HepMC::GenEvent* Evt)
+{
+  icategories[0]++;
+
+  int nisolep = LA.nIsolatedLeptons(Evt);
+
+  //std::cout << "Number of leptons = " << nisolep << std::endl;
+  if(nisolep > 0) icategories[1]++;
+  if(nisolep > 1) icategories[2]++;
+
+  JetInputHepMC::ParticleVector jetInput = JetInput(Evt);
+  std::sort(jetInput.begin(), jetInput.end(), ParticlePtGreater());
+
+  //std::cout << "Size of jet input = " << jetInput.size() << std::endl;
+
+  // Fastjet input
+  std::vector <fastjet::PseudoJet> jfInput;
+  jfInput.reserve(jetInput.size());
+  for (JetInputHepMC::ParticleVector::const_iterator iter = jetInput.begin();
+       iter != jetInput.end(); ++iter) {
+    jfInput.push_back(fastjet::PseudoJet( (*iter)->momentum().px(),
+                                          (*iter)->momentum().py(),
+                                          (*iter)->momentum().pz(),
+                                          (*iter)->momentum().e()  )  );
+    jfInput.back().set_user_index(iter - jetInput.begin());
+  }
+
+  // Run Fastjet algorithm
+  std::vector <fastjet::PseudoJet> inclusiveJets, sortedJets, cleanedJets;
+  fastjet::ClusterSequence clustSeq(jfInput, *jetDef);
+  
+  // Extract inclusive jets sorted by pT (note minimum pT in GeV)
+  inclusiveJets = clustSeq.inclusive_jets(20.0);
+  sortedJets    = sorted_by_pt(inclusiveJets);
+       
+  //std::cout << "Size of jets = " << sortedJets.size() << std::endl;
+
+  cleanedJets = LA.removeLeptonsFromJets(sortedJets, Evt);
+                                          
+  //std::cout << "Size of cleaned jets = " << cleanedJets.size() << std::endl;
+  if(nisolep > 1) {
+    if(cleanedJets.size() > 0) icategories[3]++;
+    if(cleanedJets.size() > 1) icategories[4]++;
+  }
+  
+  return ;
+
+}
+
+
+void AnalyserHepMC::endRun()
+{
+//  ofstream testi("testi.dat");
+  double val, errval;
+
+//  errval = 0.;
+//  if(icategories[0] > 0) errval = val/sqrt( (double)(icategories[0]) );
+//  testi << "pythia8_test1  1   " << val << " " << errval << " " << endl;
+
+  std::cout << std::endl;
+  std::cout << " Events with at least 1 isolated lepton  :                     "
+            << ((double)icategories[1])/((double)icategories[0]) << std::endl;
+  std::cout << " Events with at least 2 isolated leptons :                     "
+            << ((double)icategories[2])/((double)icategories[0]) << std::endl;
+  std::cout << " Events with at least 2 isolated leptons and at least 1 jet  : "
+            << ((double)icategories[3])/((double)icategories[0]) << std::endl;
+  std::cout << " Events with at least 2 isolated leptons and at least 2 jets : "
+            << ((double)icategories[4])/((double)icategories[0]) << std::endl;
+  std::cout << std::endl;
+  
+//  val = ((double)icategories[4])/((double)icategories[0]);
+//  errval = 0.;
+//  if(icategories[4] > 0) errval = val/sqrt((double)icategories[4]);
+//  testi << "pythia8_test1  2   " << val << " " << errval << " " << std::endl;
+  
+}
---
+++ generators/pythia8/tests/analyserhepmc/AnalyserHepMC.h
@@ -0,0 +1,27 @@
+#include "HepMC/GenEvent.h"
+#include "LeptonAnalyserHepMC.h"
+#include "JetInputHepMC.h"
+#include <fastjet/PseudoJet.hh>
+#include <fastjet/JetDefinition.hh>
+#include <fastjet/ClusterSequence.hh>
+
+#include <vector>
+
+
+class AnalyserHepMC {
+
+public:
+  AnalyserHepMC();
+  void initialize();
+  void analyse(const HepMC::GenEvent* pEv);
+  void endRun();
+
+private:
+  LeptonAnalyserHepMC LA;
+  JetInputHepMC JetInput;
+  fastjet::Strategy strategy;
+  fastjet::RecombinationScheme recombScheme;
+  fastjet::JetDefinition* jetDef;
+
+  int icategories[6];
+};
