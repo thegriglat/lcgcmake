7a333f381689c378b7dd20fd2a53411e946c3b9f
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2013-12-03 09:51:15 +0000

    Add Photos++-3.54 package to heptools-65/65a/66
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5603 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-66.cmake
+++ cmake/toolchain/heptools-66.cmake
@@ -194,6 +194,7 @@ LCG_external_package(pythia6           428            ${MCGENPATH}/pythia6      
 LCG_external_package(pythia6           428.2          ${MCGENPATH}/pythia6        author=6.4.28 hepevt=10000  )
 
 LCG_external_package(agile             1.4.0          ${MCGENPATH}/agile        )
+LCG_external_package(photos++          3.54           ${MCGENPATH}/photos++     )
 LCG_external_package(photos++          3.52           ${MCGENPATH}/photos++     )
 LCG_external_package(photos            215.4          ${MCGENPATH}/photos       ) 
 
