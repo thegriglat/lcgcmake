841f20c73ade3affed6626487bbb5325cdbf523c
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2013-12-03 15:44:14 +0000

    Add deleting RPATH from executables in ${INSTALL_DIR}/bin
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5605 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/RemoveRPath.cmake
+++ cmake/scripts/RemoveRPath.cmake
@@ -4,7 +4,7 @@
 
 cmake_policy(SET CMP0009 NEW) # FILE GLOB_RECURSE calls should not follow symlinks by default
 
-file(GLOB_RECURSE sharedlibs ${INSTALL_DIR}/*.so)
+file(GLOB_RECURSE sharedlibs ${INSTALL_DIR}/*.so ${INSTALL_DIR}/bin/*)
 foreach(file ${sharedlibs})
   if(NOT APPLE)
     execute_process(COMMAND stat -c "%a" "${file}" OUTPUT_VARIABLE permissions)
