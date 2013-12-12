08595b3e40cb0e2cc38ecdc3e6edbd1c6df14605
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2013-12-12 14:29:33 +0000

    Improve removing RPATH from binaries in ${INSTALL_PATH}/bin/*
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5654 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/RemoveRPath.cmake
+++ cmake/scripts/RemoveRPath.cmake
@@ -4,13 +4,17 @@
 
 cmake_policy(SET CMP0009 NEW) # FILE GLOB_RECURSE calls should not follow symlinks by default
 
-file(GLOB_RECURSE sharedlibs ${INSTALL_DIR}/*.so)
+file(GLOB_RECURSE sharedlibs ${INSTALL_DIR}/*.so ${INSTALL_DIR}/bin/*)
 foreach(file ${sharedlibs})
   if(NOT APPLE)
     execute_process(COMMAND stat -c "%a" "${file}" OUTPUT_VARIABLE permissions)
     string(REGEX REPLACE "(\r?\n)+$" "" permissions "${permissions}")
     execute_process(COMMAND chmod ugo+wx "${file}")
-    file(RPATH_REMOVE FILE "${file}")
+    set (check_rpath 0)
+    execute_process(COMMAND ${CMAKE_SOURCE_DIR}/check_relative_rpath.sh "${file}" RESULT_VARIABLE check_rpath)
+    if (check_rpath)
+        file(RPATH_REMOVE FILE "${file}")
+    endif()
     execute_process(COMMAND chmod ${permissions} "${file}")
   endif()
 endforeach()
---
+++ cmake/scripts/check_relative_rpath.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# 
+# Script checks RPATH relative or not
+# if yes -- exit with code $REMOVE
+# if no  -- exit with code $KEEP
+# 11.12.2013
+#
+
+KEEP=0
+REMOVE=1
+
+function check_rpath(){
+    echo $1 | grep -E -q '\.\.\/.*|\$' && return $KEEP || return $REMOVE
+}
+
+file=$1
+
+file $file | grep -v -q ELF && exit $KEEP
+
+if echo $file | grep -E -q "\.so"; then
+    exit $REMOVE
+fi
+
+rpath=`readelf -d $file | grep -i rpath | cut -d[ -f2 | cut -d] -f1`
+result=0
+for path in `echo $rpath | sed 's/:/ /g'`; do
+    check_rpath $path
+    let "result = $result + $?"
+done
+[ $result -gt 0 ] && result=$REMOVE
+
+exit $result
