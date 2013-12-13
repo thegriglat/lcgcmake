ebc47d4c4750c1dcdd79d0f844bb7ddc6092c4ae
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2013-12-13 14:20:13 +0000

    Fix printing
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5662 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/check_debug_symbols.sh
+++ cmake/scripts/check_debug_symbols.sh
@@ -11,16 +11,10 @@
 ROOT_PATH=$1
 echo "ROOT_PATH=$ROOT_PATH"
 exitcode=0
-total=0
-debfile=0
 find $ROOT_PATH -type f -name *.so | while read name; do
     debug=`readelf --debug-dump $name 2> /dev/null`
     if [ "x$debug" != "x" ];then
-        echo "WARNING: File $name consists debug symbols."
-        let "debfile = $debfile + 1"
+        echo "WARNING: File $name contains debug symbols."
     fi
-    let "total = $total + 1"
 done
-echo "Total *.so files         : $total"
-echo "Files with debug symbols : $debfile"
 exit $exitcode
