eb6329f4fef3f675942ccf0bfe6c38bf535c4cef
gianolio <gianolio@4525493e-7705-40b1-a816-d608a930855b>
2013-12-18 10:47:26 +0000

    create the external volume if does not exist when I ask to create the generators's one
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5697 4525493e-7705-40b1-a816-d608a930855b

--- cdash/createAFSvolumeScripts/prepareAFSReleaseArea.py
+++ cdash/createAFSvolumeScripts/prepareAFSReleaseArea.py
@@ -19,25 +19,21 @@ if __name__ == '__main__':
     (opts,args) = parser.parse_args()
     if not opts.version or not opts.basepath or not opts.target:
         print "You have to specify all --prefix, --version and --target"
-        sys.exit(1)
-        #        raise ValueError("You have to specify both --prefix and --version")
+        raise ValueError("You have to specify both --prefix and --version")
     if not os.path.exists(opts.basepath):
         print ("You specified a non existing path in --prefix")
-        sys.exit(1)
-        # raise ValueError("You have to specify an existing path")
+        raise ValueError("You have to specify an existing path")
 
     # here it is decided the pattern of the release i.e. lcg65 or lcg_65 ...
     releasename_pattern = r"lcg\d+[a-z]?"
 
     if not re.match(releasename_pattern, opts.version):
         print "the name of the release is not standard"
-        sys.exit(1)
-        # raise ValueError("You have to use a standard release name ")
+        raise ValueError("You have to use a standard release name (lcg+number[+letter] ")
     if opts.target not in ('all', 'externals', 'generators'): 
         print "target must be one of all|externals|generators"
         print "target : ",opts.target
-        sys.exit(1)
-        # raise ValueError("target must be either all or externals or generators")
+        raise ValueError("target must be either all or externals or generators")
 
     area = ProjectAreaManager( opts.version, opts.basepath, dry=opts.dry )
 
@@ -46,13 +42,15 @@ if __name__ == '__main__':
     if opts.target == "externals": 
         print "create %s area"  % os.path.join(opts.basepath,opts.version) 
         area.create_area(releasename_pattern, "")
-    elif opts.target == 'generators':
-        print "create %s area - if %s exists" % (os.path.join(opts.basepath,opts.version,"MCGenerators_"+opts.version), os.path.join(opts.basepath,opts.version))
-        area.create_area(releasename_pattern, "MCGenerators_"+opts.version)
-    else: # opts.target == all
-        print "create %s area and %s area" % (os.path.join(opts.basepath,opts.version), (os.path.join(opts.basepath,opts.version,"MCGenerators_"+opts.version)))
-        area.create_area(releasename_pattern, "")
-        cmd = '/usr/bin/fs checks; /usr/bin/fs checkv; /usr/bin/fs flushm '+os.path.join(opts.basepath,opts.version)
-        os.system(cmd)
-        area.create_area(releasename_pattern, "MCGenerators_"+opts.version)
+    else: 
+        if os.path.exists(os.path.join(opts.basepath,opts.version)):
+            if opts.target == 'generators':
+                print "create %s area - if %s exists" % (os.path.join(opts.basepath,opts.version,"MCGenerators_"+opts.version), os.path.join(opts.basepath,opts.version))
+                area.create_area(releasename_pattern, "MCGenerators_"+opts.version)
+        else: # opts.target == all or opts.target == generatos but lcg volume does not exist
+            print "create %s area and %s area" % (os.path.join(opts.basepath,opts.version), (os.path.join(opts.basepath,opts.version,"MCGenerators_"+opts.version)))
+            area.create_area(releasename_pattern, "")
+            cmd = '/usr/bin/fs checks; /usr/bin/fs checkv; /usr/bin/fs flushm '+os.path.join(opts.basepath,opts.version)
+            os.system(cmd)
+            area.create_area(releasename_pattern, "MCGenerators_"+opts.version)
 
