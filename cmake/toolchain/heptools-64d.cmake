d5e0ba461a68ef31999a371cdb925d906fc30128
dkonst <dkonst@4525493e-7705-40b1-a816-d608a930855b>
2014-02-07 09:43:53 +0000

    cython added to 64d
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5809 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-64d.cmake
+++ cmake/toolchain/heptools-64d.cmake
@@ -222,6 +222,7 @@ LCG_external_package(pythia8           175.lhetau     ${MCGENPATH}/pythia8      
 
 LCG_external_package(starlight         r43            ${MCGENPATH}/starlight    )
 
+CG_external_package(cython             0.19.1         ${MCGENPATH}/cython       )
 LCG_external_package(yamlcpp           0.3.0          ${MCGENPATH}/yamlcpp      ) 
 LCG_external_package(yoda              1.0.5          ${MCGENPATH}/yoda       )
 LCG_external_package(rivet             2.1.0          ${MCGENPATH}/rivet        yoda=1.0.5      )
