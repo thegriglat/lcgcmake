30a6df0c8e70557f3866209e64016618034a0267
dkonst <dkonst@4525493e-7705-40b1-a816-d608a930855b>
2014-02-16 15:17:19 +0000

    sacrifice to 61f and 64d
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5816 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-61f.cmake
+++ cmake/toolchain/heptools-61f.cmake
@@ -183,6 +183,8 @@ LCG_external_package(rivet             2.1.0          ${MCGENPATH}/rivet        
 LCG_external_package(rivet             1.9.0          ${MCGENPATH}/rivet        yoda=1.0.5       )
 LCG_external_package(rivet             1.8.3          ${MCGENPATH}/rivet        yoda=1.0.5       )
 
+LCG_external_package(sacrifice         0.9.9          ${MCGENPATH}/sacrifice    )
+
 LCG_external_package(sherpa            2.0.0          ${MCGENPATH}/sherpa         author=2.0.0 hepevt=10000  )
 
 LCG_external_package(sherpa            1.4.3          ${MCGENPATH}/sherpa         author=1.4.3 hepevt=4000  )
