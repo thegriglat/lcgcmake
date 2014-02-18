032abc68e5e78cffb9c80e714ba4f1cd0f2418bd
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-02-18 17:37:48 +0000

    add postinstall script for qmtest to make it relocatable
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5820 4525493e-7705-40b1-a816-d608a930855b

---
+++ externals/qmtest_postinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#post-install macro to make QMtest relocatable 
+
+# 1) replace absolute pythonpath by relative one in the executable
+location="\#\!\/usr\/bin\/env python"
+
+sed -i "1s/.*/$location/" $1/bin/qmtest
+
+# 2) fix config.py to be self-relative
+cd $1/lib/python*/site-packages/qm
+repl=`grep prefix config.py`
+rm config.pyc
+mv config.py config.py.org
+echo "import os" > config.py
+sed s%$repl%prefix=os.path.realpath\(os.path.abspath\(os.path.dirname\(__file__\)\)+\(os.sep+os.pardir\)*4\)% config.py.org >> config.py
+echo >> config.py
