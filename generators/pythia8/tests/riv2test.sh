9b7d0c40bfe38ce44508dbc8e0bad3e1aaa22f8e
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2014-03-05 18:01:45 +0000

    add XS analysis
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5860 4525493e-7705-40b1-a816-d608a930855b

--- generators/pythia8/tests/riv2test.sh
+++ generators/pythia8/tests/riv2test.sh
@@ -6,5 +6,5 @@ echo $2
 echo end arguments
 
 source $1/rivetenv.sh
-rivet -a MC_ZJETS --histo-file=pythia8_riv2_i.yoda $2
+rivet -a MC_XS -a MC_ZJETS --histo-file=pythia8_riv2_i.yoda $2
 #rivet -a CMS_2012_I1184941 $2
