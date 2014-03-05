60c152eade0e48157a4daa9b9972d5410997b6de
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-03-05 10:07:53 +0000

    add empty backup option to sed; required by Macos
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5854 4525493e-7705-40b1-a816-d608a930855b

--- externals/GCCXML_postinstall.sh
+++ externals/GCCXML_postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #post-install macro to make gccxml_config relocatable 
 
-sed -i "1s/.*/GCCXML_COMPILER=\"c\+\+\"/" $1/share/gccxml-0.9/gccxml_config
+sed -i "" "1s/.*/GCCXML_COMPILER=\"c\+\+\"/" $1/share/gccxml-0.9/gccxml_config
 
--- externals/Python_postinstall.sh
+++ externals/Python_postinstall.sh
@@ -4,5 +4,5 @@
 # replace absolute pythonpath by relative one
 location="\#\!\/usr\/bin\/env python"
 
-sed -i "1s/.*/$location/" $1/bin/python-config
+sed -i "" "1s/.*/$location/" $1/bin/python-config
 
