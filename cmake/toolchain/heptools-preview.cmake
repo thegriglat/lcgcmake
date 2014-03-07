030f92169e85ae74bfc834f5e1b6264b3cede3f9
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-03-07 12:48:54 +0000

    upgrade project versions in preview
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5868 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-preview.cmake
+++ cmake/toolchain/heptools-preview.cmake
@@ -15,8 +15,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/heptools-common.cmake)
 LCG_AA_project(COOL  COOL_2_9_1)
 LCG_AA_project(CORAL CORAL_2_4_1)
 LCG_AA_project(RELAX RELAX_1_3_0p)
-LCG_AA_project(ROOT  5.34.13)
-
 LCG_AA_project(ROOT  5.34.99)
 LCG_AA_project(LCGCMT LCGCMT-${heptools_version})
 
