277b41e87e0c20e2da21171aaf403584bd7349ae
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2014-03-11 14:55:45 +0000

    rivet 2.1.1 and yoda 1.0.6 added
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5885 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-67b.cmake
+++ cmake/toolchain/heptools-67b.cmake
@@ -212,6 +212,7 @@ if(NOT ${LCG_OS}${LCG_OSVERS} STREQUAL mac109) # rivet 2 does not work yet with 
   LCG_external_package(rivet             2.0.0         ${MCGENPATH}/rivet	yoda=1.0.4      )
 endif()
 LCG_external_package(rivet             2.1.0          ${MCGENPATH}/rivet        yoda=1.0.5      )
+LCG_external_package(rivet             2.1.1          ${MCGENPATH}/rivet        yoda=1.0.6      )
 
 LCG_external_package(rivet             1.8.3          ${MCGENPATH}/rivet yoda=1.0.4       )
 LCG_external_package(rivet             1.9.0          ${MCGENPATH}/rivet yoda=1.0.4      )
@@ -234,6 +235,7 @@ LCG_external_package(cython            0.19.1         ${MCGENPATH}/cython       
 
 LCG_external_package(yamlcpp           0.3.0          ${MCGENPATH}/yamlcpp      ) 
 
+LCG_external_package(yoda              1.0.6          ${MCGENPATH}/yoda         )
 LCG_external_package(yoda              1.0.5          ${MCGENPATH}/yoda         )
 LCG_external_package(yoda              1.0.4          ${MCGENPATH}/yoda         ) 
 
