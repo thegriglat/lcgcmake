3ac6fbce070002fe638d3c402df40ede3c349812
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2014-03-14 15:46:23 +0000

    update
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5893 4525493e-7705-40b1-a816-d608a930855b

---
+++ generators/sherpa/tests/riv2test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo arguments:
+echo $1
+echo $2
+echo end arguments
+
+source $1/rivetenv.sh
+rivet -a MC_XS -a MC_ZJETS --histo-file=sherpa_riv2_i.yoda $2
+#rivet -a CMS_2012_I1184941 $2
