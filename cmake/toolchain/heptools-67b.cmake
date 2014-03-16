4f42d6ef2da31146487732ec56f13c777a701c4a
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2014-03-16 12:29:29 +0000

    sherpa 2.1.0 to be tested
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5900 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-67b.cmake
+++ cmake/toolchain/heptools-67b.cmake
@@ -217,9 +217,9 @@ LCG_external_package(rivet             2.1.1          ${MCGENPATH}/rivet        
 LCG_external_package(rivet             1.8.3          ${MCGENPATH}/rivet yoda=1.0.4       )
 LCG_external_package(rivet             1.9.0          ${MCGENPATH}/rivet yoda=1.0.4      )
 
-LCG_external_package(sherpa            2.1.0          ${MCGENPATH}/sherpa         author=2.1.0 hepevt=10000)
-LCG_external_package(sherpa            2.0.0          ${MCGENPATH}/sherpa         author=2.0.0 hepevt=10000)
 LCG_external_package(sherpa            1.4.5.2        ${MCGENPATH}/sherpa         author=1.4.5 hepevt=10000 )
+LCG_external_package(sherpa            2.0.0          ${MCGENPATH}/sherpa         author=2.0.0 hepevt=10000)
+LCG_external_package(sherpa            2.1.0          ${MCGENPATH}/sherpa         author=2.1.0 hepevt=10000)
 
 LCG_external_package(hepmcanalysis     3.4.14         ${MCGENPATH}/hepmcanalysis  author=00-03-04-14        )
 LCG_external_package(mctester          1.25.0         ${MCGENPATH}/mctester     )
