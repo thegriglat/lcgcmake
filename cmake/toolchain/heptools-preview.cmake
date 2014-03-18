632082af40f1e4d33327ce110a878aba8f6ad745
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-03-18 16:26:27 +0000

    add xrootd_python to externals in preview
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5910 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-preview.cmake
+++ cmake/toolchain/heptools-preview.cmake
@@ -112,6 +112,7 @@ LCG_external_package(vdt               0.3.6                                    
 LCG_external_package(XercesC           3.1.1p1        author=3.1.1              )
 LCG_external_package(xqilla            2.2.4p1                                  )
 LCG_external_package(xrootd            3.3.6                                    )
+LCG_external_package(xrootd_python     0.1.3                                    )
 
 #---EMI-2 grid externals and other binary packages---------------------
 if(NOT ${LCG_OS} STREQUAL mac)
