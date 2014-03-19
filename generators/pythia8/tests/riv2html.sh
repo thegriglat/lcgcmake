7c85ee87a3e7d557a89fc09967a696c144ac929a
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2014-03-19 17:57:15 +0000

    script for visualisation of the histograms comparison
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5932 4525493e-7705-40b1-a816-d608a930855b

---
+++ generators/pythia8/tests/riv2html.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo arguments:
+echo $1
+echo $2
+echo $3
+echo end arguments
+
+source $1/rivetenv.sh
+
+rivet-mkhtml --help
+
+rivet-mkhtml -o pythia8html -s --mc-errs pythia8_riv2.yoda:'Title=Reference' pythia8_riv2_i.yoda:'LineColor=blue'
