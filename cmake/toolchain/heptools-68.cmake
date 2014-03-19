940d64285a1dcba37b5ea2bf69317bccbe8e305b
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-03-19 16:49:38 +0000

    upgrade CORAL and COOL versions for LCG 68
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5930 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-68.cmake
+++ cmake/toolchain/heptools-68.cmake
@@ -12,8 +12,8 @@ include(${CMAKE_CURRENT_LIST_DIR}/heptools-common.cmake)
 
 
 # Application Area Projects
-LCG_AA_project(COOL  COOL_2_9_1)
-LCG_AA_project(CORAL CORAL_2_4_1)
+LCG_AA_project(COOL  COOL_2_9_2)
+LCG_AA_project(CORAL CORAL_2_4_2)
 LCG_AA_project(RELAX RELAX_1_3_0p)
 LCG_AA_project(ROOT  5.34.18)
 LCG_AA_project(LCGCMT LCGCMT-${heptools_version})
