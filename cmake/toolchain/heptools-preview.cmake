2c8373e51f4e2a6b28a52cc823996c49a03035eb
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-03-19 08:40:47 +0000

    prepare ROOT checkout for branch handling; ad C++11
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@5914 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-preview.cmake
+++ cmake/toolchain/heptools-preview.cmake
@@ -15,7 +15,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/heptools-common.cmake)
 LCG_AA_project(COOL  COOL_2_9_1)
 LCG_AA_project(CORAL CORAL_2_4_1)
 LCG_AA_project(RELAX RELAX_1_3_0p)
-LCG_AA_project(ROOT  5.34.99)
+LCG_AA_project(ROOT  v5-34-00-patches)
 LCG_AA_project(LCGCMT LCGCMT-${heptools_version})
 
 # Compilers
