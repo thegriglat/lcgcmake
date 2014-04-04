ddf5b298c54ae04c5acd8b7b9fc1069a74804692
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2014-04-04 17:21:45 +0000

    add generator version to the stored yoda file name
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6038 4525493e-7705-40b1-a816-d608a930855b

--- generators/reference/riv2html.sh
+++ generators/reference/riv2html.sh
@@ -3,23 +3,24 @@
 echo arguments:
 echo $1 - path to rivet
 echo $2 - generator name
-echo $3 - process name
-echo $4 - LCG version
-echo $5 - platform
+echo $3 - generator version
+echo $4 - process name
+echo $5 - LCG version
+echo $6 - platform
 echo end arguments
 
 RESULTSTORE=/afs/cern.ch/sw/lcg/external/MCGenerators_test/validation
 
 mkdir -p ${RESULTSTORE}/yoda
-cp -f ${2}_riv2_${3}.yoda ${RESULTSTORE}/yoda/${2}_${3}_ref.yoda
-cp -f ${2}_riv2_${3}_i.yoda ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}.yoda
+cp -f ${2}_riv2_${4}.yoda ${RESULTSTORE}/yoda/${2}_${4}_ref.yoda
+cp -f ${2}_riv2_${4}_i.yoda ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}_${6}.yoda
 
 echo
 echo yoda file with results is produced and put together with the reference on AFS
 echo
 echo to get html page with comparison in the directory ./myhtml run the following:
 echo
-echo ${RESULTSTORE}/runmkhtml.sh ${RESULTSTORE}/yoda/${2}_${3}_ref.yoda ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}.yoda myhtml
+echo ${RESULTSTORE}/runmkhtml.sh ${RESULTSTORE}/yoda/${2}_${4}_ref.yoda ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}_${6}.yoda myhtml
 echo
 echo after this you can see the page by typing:
 echo
@@ -27,4 +28,4 @@ echo firefox myhtml/index.html
 echo
 
 #source $1/rivetenv.sh
-#rivet-mkhtml -o ${2}html -s --mc-errs ${2}_riv2.yoda:'Title=Reference' ${2}_riv2_i.yoda:'LineColor=blue'
+#rivet-mkhtml -o ${2}html -s --mc-errs ${2}_riv2_${4}.yoda:'Title=Reference' ${2}_riv2_${4}_i.yoda:'LineColor=blue'
