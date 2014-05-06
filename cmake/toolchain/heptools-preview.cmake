8202face326d1a0e9f983aaf7b765505b6d1b211
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-05-06 12:37:43 +0000

    fix version of CORAL/COOL
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6095 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-preview.cmake
+++ cmake/toolchain/heptools-preview.cmake
@@ -12,8 +12,8 @@ include(${CMAKE_CURRENT_LIST_DIR}/heptools-common.cmake)
 
 
 # Application Area Projects
-LCG_AA_project(COOL  COOL_2_9_1)
-LCG_AA_project(CORAL CORAL_2_4_1)
+LCG_AA_project(COOL  COOL_2_9_2)
+LCG_AA_project(CORAL CORAL_2_4_2)
 LCG_AA_project(RELAX RELAX_1_3_0p)
 LCG_AA_project(ROOT  v5-34-00-patches)
 LCG_AA_project(LCGCMT LCGCMT_${heptools_version})
