1c5fbabd735f9a0451cd0e6c302cb0ebaa3136e6
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-05-12 19:27:05 +0000

    add AFS handling script
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6108 4525493e-7705-40b1-a816-d608a930855b

---
+++ cdash/copy_to_afs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# This script copies build results to AFS in case of nightlies
+
+import os
+import datetime
+import subprocess
+import sys
+
+def today():
+    return datetime.date.today().strftime('%a')
+
+def get_platform():
+    return subprocess.Popen("cd /ec/lcg-scripts/; cmake -P /ec/lcg-scripts/get_platform.cmake; cd $OLDPWD", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read().rstrip()
+
+##########################
+if __name__ == "__main__":
+
+  # first we check whether we want to do anything at all
+  slotname = os.environ.get('SLOTNAME')
+  if slotname in (None, "none") :
+    sys.exit(0)
+
+  # define source and target  
+  workdir = os.environ['WORKDIR']
+  sourcedir = "%s/%s-install" %(workdir,get_platform())
+  targetdir = "/afs/cern.ch/sw/lcg/app/nightlies/%s/%s" %(slotname,today())
+  command = "rsync -au %s/ %s" %(sourcedir,targetdir)
+  print "Executing %s" %command
+  print subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+  command = "touch %s/isDone-%s" %(targetdir,get_platform())
+  print subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+    
+
+
+    
+  
