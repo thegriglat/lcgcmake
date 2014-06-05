ba1d1fa35a7797a37f6bbbc74e1a32c968864cc0
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-06-05 18:07:30 +0000

    fix typo in COOL tag
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6178 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-experimental.cmake
+++ cmake/toolchain/heptools-experimental.cmake
@@ -12,7 +12,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/heptools-common.cmake)
 
 
 # Application Area Projects
-LCG_AA_project(COOL  COOL_3_0_0-preview)
+LCG_AA_project(COOL  COOL_3_0-preview)
 LCG_AA_project(CORAL CORAL_3_0-preview)
 LCG_AA_project(RELAX RELAX-root6)
 LCG_AA_project(ROOT  HEAD)
