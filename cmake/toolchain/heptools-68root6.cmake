21de9dacc902e4d353faa4593e15535393e512d7
dkonst <dkonst@4525493e-7705-40b1-a816-d608a930855b>
2014-06-24 12:40:06 +0000

    68 and 68root6 updated by crmc-1.4
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6202 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-68root6.cmake
+++ cmake/toolchain/heptools-68root6.cmake
@@ -220,6 +220,7 @@ LCG_external_package(starlight         r43            ${MCGENPATH}/starlight    
 LCG_external_package(herwig            6.520.2        ${MCGENPATH}/herwig       )
 LCG_external_package(herwig            6.521.2        ${MCGENPATH}/herwig       )
 
+LCG_external_package(crmc              1.4            ${MCGENPATH}/crmc         )
 LCG_external_package(crmc              1.3            ${MCGENPATH}/crmc         )
 LCG_external_package(cython            0.19.1         ${MCGENPATH}/cython       )
 LCG_external_package(yamlcpp           0.3.0          ${MCGENPATH}/yamlcpp      )
