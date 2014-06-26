e951e6d436d7d6725d223bca3328d6a2ef71341f
dkonst <dkonst@4525493e-7705-40b1-a816-d608a930855b>
2014-06-26 10:12:46 +0000

    distclean target added to vincia build systme
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6214 4525493e-7705-40b1-a816-d608a930855b

---
+++ generators/vincia/files/cmake/modules/TargetDistclean.cmake
@@ -0,0 +1,26 @@
+IF (UNIX)
+  ADD_CUSTOM_TARGET (distclean @echo cleaning for source distribution)
+  SET(DISTCLEANED
+   cmake.depends
+   cmake.check_depends
+   CMakeCache.txt
+   cmake.check_cache
+   *.cmake
+   Makefile
+   core core.*
+   gmon.out
+   CMakeFiles
+   vicniaenv*.*sh
+   install_manifest.txt
+   *~
+  )
+  
+  ADD_CUSTOM_COMMAND(
+    DEPENDS clean
+    COMMENT "distribution clean"
+    COMMAND rm
+    ARGS    -Rf CMakeTmp ${DISTCLEANED}
+    TARGET  distclean
+  )
+ENDIF(UNIX)
+
