9b529195cd4037cce320ea88913aa3077c82d9b3
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-06-30 22:46:26 +0000

    add InstallArea functionality
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6226 4525493e-7705-40b1-a816-d608a930855b

--- cdash/copy_to_afs.py
+++ cdash/copy_to_afs.py
@@ -12,6 +12,70 @@ def today():
 def get_platform():
     return subprocess.Popen("cd /ec/lcg-scripts/; cmake -P /ec/lcg-scripts/get_platform.cmake; cd $OLDPWD", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read().rstrip()
 
+# part to handle an InstallArea inside LCGCMT with the following structure:
+# LCGCMT/<version>/InstallArea/<platform>/
+#    bin/
+#    lib/
+# with symlinks to all binaries and .so files defined by that LCGCMT nightly build
+blacklist = ['src','sources','boost','share','include','.svn','doc',"MCGenerators",'logs','etc','CMT']
+def removeBlackListed(dirs):
+    for item in blacklist:
+      try:
+        dirs.remove(item)
+      except ValueError:
+        pass
+
+def identifyLib(root,aFile,platform):
+    if (root.endswith("lib") or root.endswith("lib64")):
+      if ".so" in aFile or aFile.endswith("rootmap"):
+        if platform in root:
+          return True
+    return False
+      
+def identifyBin(root,aFile,platform):
+    if root.endswith("bin"):
+      if platform in root:
+        return True
+    return False
+
+
+def createInstallArea(basepath,platform,lcgversion):
+  # create InstallArea dirs
+  installarea = os.path.join(basepath,"LCGCMT",lcgversion,"InstallArea",platform)
+  if not os.path.exists(installarea):
+    os.makedirs(installarea)
+    os.makedirs(os.path.join(installarea,"lib"))
+    os.makedirs(os.path.join(installarea,"bin"))
+  print installarea, "created"
+
+  # collect all libs and bins for the given platform
+  libs = {}
+  bins = {}
+  for root, dirs, files in os.walk(basepath):
+     # remove blacklisted subdirs
+     removeBlackListed(dirs)
+     for afile in files:
+       if identifyLib(root,afile,platform): 
+         libs[afile] = os.path.join(root,afile)
+       elif identifyBin(root,afile,platform):
+         bins[afile] = os.path.join(root,afile)
+
+  # create symlinks
+  os.chdir(os.path.join(installarea,"lib"))
+  for key, value in libs.iteritems():
+    destination = os.path.relpath(value)
+    if not os.path.exists(key):
+      os.symlink(destination,key)
+      print "Creating %s" %(destination)
+  os.chdir(os.path.join(installarea,"bin"))
+  for key, value in bins.iteritems():
+    destination = os.path.relpath(value)
+    if not os.path.exists(key):   
+       os.symlink(destination,key)
+       print "Creating %s" %(destination) 
+
+
+
 ##########################
 if __name__ == "__main__":
 
@@ -30,6 +94,9 @@ if __name__ == "__main__":
   print subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
   command = "touch %s/isDone-%s" %(targetdir,get_platform())
   print subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+
+  #create InstallArea
+  createInstallArea(targetdir,get_platform(),"LCGCMT_69root6") #TODO: make version dynamic 
     
 
 
