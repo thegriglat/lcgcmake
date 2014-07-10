3dbfe47a35eecf427fce46ac681a4fd17e1ced53
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2014-07-10 09:53:26 +0000

    Up pythia8-999 in preview
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6263 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-preview.cmake
+++ cmake/toolchain/heptools-preview.cmake
@@ -171,8 +171,8 @@ LCG_external_package(powheg-box         r2092         ${MCGENPATH}/powheg-box   
 
     LCG_external_package(pythia8           175            ${MCGENPATH}/pythia8    author=175  )
     LCG_external_package(pythia8           183            ${MCGENPATH}/pythia8    author=183  )
-    LCG_external_package(pythia8           185            ${MCGENPATH}/pythia8    author=185  )
     LCG_external_package(pythia8           999            ${MCGENPATH}/pythia8    author=999  )
+    LCG_external_package(pythia8           185            ${MCGENPATH}/pythia8    author=185  )
 
     LCG_external_package(sacrifice         0.9.9          ${MCGENPATH}/sacrifice pythia8=185)
 
