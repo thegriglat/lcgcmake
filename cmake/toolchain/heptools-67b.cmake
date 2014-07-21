19da328368d74d26c413449f0105ea033ad217ce
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2014-07-21 08:38:22 +0000

    Add pythia8-186 to 67b release
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6311 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-67b.cmake
+++ cmake/toolchain/heptools-67b.cmake
@@ -180,6 +180,7 @@ LCG_external_package(pythia8           175.lhetau     ${MCGENPATH}/pythia8      
 LCG_external_package(pythia8           175            ${MCGENPATH}/pythia8      author=175     )
 LCG_external_package(pythia8           183            ${MCGENPATH}/pythia8      author=183     )
 LCG_external_package(pythia8           185            ${MCGENPATH}/pythia8      author=185     )
+LCG_external_package(pythia8           186            ${MCGENPATH}/pythia8      author=186     )
 LCG_external_package(pythia8           180            ${MCGENPATH}/pythia8      author=180     )
 
 LCG_external_package(sacrifice         0.9.9          ${MCGENPATH}/sacrifice  pythia8=185 )
