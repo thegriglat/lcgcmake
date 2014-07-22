f213c49c3642a645b8c0320aba1c8d66e5c39997
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2014-07-22 19:59:59 +0000

    Effort to optimize check_debug_symbols.sh
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6323 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/check_debug_symbols.sh
+++ cmake/scripts/check_debug_symbols.sh
@@ -6,29 +6,23 @@
 # Only 'real' libraries are checked, symlinks are skipped.
 # To change this, remove '-type f' in line 16
 #
-# 12.12.2013
 
 ROOT_PATH=$1
 OS=$(uname)
-[ "Darwin" == $OS ] && filepattern="-name *.dylib" || filepattern="-name *.so"
+if [ "Darwin" == $OS ];then
+  filepattern="-name *.dylib"
+  chkcmd="mac_check"
+else
+  filepattern="-name *.so"
+  chkcmd="slc_check"
+fi
 
 slc_check(){
     readelf --debug-dump "$1" 2> /dev/null
 }
 
 mac_check(){
-    symbols -onlyDebugMapData "$1" | grep -vE "DATA|TEXT" | tail -n +4
-}
-
-check(){
-    if [ "Darwin" == $OS ];then
-        mac_check "$1"
-    elif [ "Linux" == $OS ]; then
-        slc_check "$1"
-    else
-        echo "Cannot determine Operating System: $OS."
-        exit 1
-    fi
+    symbols -onlyDebugMapData "$1" | grep -vE "DATA|TEXT" | tail -n +4 2> /dev/null
 }
 
 echo "Checking debug symbols in ${ROOT_PATH}/* ..."
@@ -37,8 +31,8 @@ if echo ${ROOT_PATH} | grep -q '\-opt'; then
     echo "The following libraries have debug symbols:"
     for dirs in $(find $ROOT_PATH -maxdepth 4 -type d); do
       for name in $([ -d "$dirs/lib" ] && find $dirs/lib -type f $filepattern) $([ -d "$dirs/bin" ] && find $dirs/bin -type f $filepattern); do
-        debug=$(check "$name")
-        if [ "x$debug" != "x" ];then
+        $chkcmd $1 | wc -l | grep -q -w 0
+        if [ $? -ne 0 ];then
             echo "WARNING: File $name contains debug symbols."
             echo "$name" | grep -q -i MCGenerators && echo "$name" | grep -q -v site-packages && exitcode=1
         fi
@@ -48,10 +42,7 @@ elif echo ${ROOT_PATH} | grep -q '\-dbg'; then
     echo "The following libraries don't have debug symbols:"
     for dirs in $(find $ROOT_PATH -maxdepth 4 -type d); do
       for name in $([ -d "$dirs/lib" ] && find $dirs/lib -type f $filepattern | grep '\-dbg') $([ -d "$dirs/bin" ] && find $dirs/bin -type f $filepattern | grep '\-dbg'); do
-        debug=$(check "$name")
-        if [ "x$debug" = "x" ];then
-            echo "WARNING: File $name doesn't contain debug symbols."
-        fi
+        $chkcmd $1 | wc -l | grep -q -w 0 && echo "WARNING: File $name doesn't contain debug symbols."
       done
     done
 fi
