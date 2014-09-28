1c652604d87ccd582c941e25a22d397339a46f3d
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-09-28 19:00:10 +0000

    use fixed HepMC version in experimental; should get closer to release
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6517 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-experimental.cmake
+++ cmake/toolchain/heptools-experimental.cmake
@@ -17,7 +17,7 @@ LCG_AA_project(CORAL CORAL_3_0-preview)
 LCG_AA_project(RELAX RELAX-root6)
 LCG_AA_project(ROOT  v6-02-00-patches)
 LCG_AA_project(LCGCMT LCGCMT_${heptools_version})
-LCG_AA_project(HepMC githead)
+LCG_AA_project(HepMC 2.06.08)
 LCG_external_package(Geant4 9.6.p03 CLHEP=2.1.4.1)
 
 # Compilers
