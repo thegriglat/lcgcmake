1c7c183d6ccdb63fa7997d8b152cfdb145546742
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-10-06 14:43:31 +0000

    fix LCGCMT version
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6559 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-69.cmake
+++ cmake/toolchain/heptools-69.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 2.8.5)
 # Declare the version of HEP Tools we use
 # (must be done before including heptools-common to allow evolution of the
 # structure)
-set(heptools_version  preview)
+set(heptools_version 69)
 
 include(${CMAKE_CURRENT_LIST_DIR}/heptools-common.cmake)
 
@@ -16,7 +16,7 @@ LCG_AA_project(COOL  COOL_2_9_3)
 LCG_AA_project(CORAL CORAL_2_4_3)
 LCG_AA_project(RELAX RELAX_1_3_0p)
 LCG_AA_project(ROOT  5.34.21)
-LCG_AA_project(LCGCMT LCGCMT_${heptools_version})
+LCG_AA_project(LCGCMT LCGCMT_69)
 LCG_AA_project(HepMC 2.06.08)
 
 # Compilers
