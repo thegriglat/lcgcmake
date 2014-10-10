43ec4686956d6138e0d9eef33451f49f0f30e077
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2014-10-10 18:02:36 +0000

    change for safety in pdfsets.cmake and obligatory set added
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6576 4525493e-7705-40b1-a816-d608a930855b

--- generators/lhapdf6sets/pdfsets.cmake
+++ generators/lhapdf6sets/pdfsets.cmake
@@ -1,30 +1,31 @@
 # Arguments:
 #   distr   - path to LHAPDF source package
 #   repo    - URL of PDF sets repository
-#   PDFsets - list of PDF sets to install
+#   PDFS    - list of PDF sets to install
 #   dst_dir - installation directory
 
 message(STATUS "Distributive: ${distr}")
 message(STATUS "PDF repo: ${repo}")
-message(STATUS "PDF sets: ${PDFsets}")
+message(STATUS "PDF sets: ${PDFS}")
 message(STATUS "Destination: ${dst_dir}")
 
 # PDF sets directory
 set(pdf_dir ${dst_dir})
 message(STATUS "PDF sets directory: ${pdf_dir}")
 
+if(NOT EXISTING_DIR MATCHES "YES")
 if(EXISTS ${pdf_dir})
   message(STATUS "PDF sets directory exists already, skip download")
   return()
+endif()
 endif()
 
 file(MAKE_DIRECTORY ${pdf_dir})
 
 # download PDF sets
-message(STATUS "the following command will be executed: ${distr}/bin/lhapdf-getdata --force --repo=${repo} --dest=${pdf_dir} ${PDFsets}  ")
-execute_process(COMMAND ${distr}/bin/lhapdf-getdata --untar --force --repo=${repo} --dest=${pdf_dir} ${PDFsets} RESULT_VARIABLE code)
+message(STATUS "the following command will be executed: ${distr}/bin/lhapdf-getdata --force --repo=${repo} --dest=${pdf_dir} ${PDFS}  ")
+execute_process(COMMAND ${distr}/bin/lhapdf-getdata --untar --force --repo=${repo} --dest=${pdf_dir} ${PDFS} cteq6l1 RESULT_VARIABLE code)
 execute_process(COMMAND ${distr}/bin/lhapdf-getdata --force --repo=${repo} --dest=${pdf_dir} pdfsets.index RESULT_VARIABLE code)
-
 
 if(NOT code EQUAL 0)
   message(FATAL_ERROR "ERROR: failed to download PDF sets")
