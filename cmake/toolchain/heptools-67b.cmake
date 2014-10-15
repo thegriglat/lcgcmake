dfe419c40991dae1af3a821f2c6ec086134ad01f
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2014-10-15 09:01:54 +0000

    Add pythia8 to 67b
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6593 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-67b.cmake
+++ cmake/toolchain/heptools-67b.cmake
@@ -182,11 +182,12 @@ LCG_external_package(lhapdf6           6.0.5          ${MCGENPATH}/lhapdf6      
 LCG_external_package(lhapdf6sets       6.0.0          lhapdf6sets   )
 
 LCG_external_package(pythia8           175.lhetau     ${MCGENPATH}/pythia8      author=175     )
-LCG_external_package(pythia8           175            ${MCGENPATH}/pythia8      author=175     )
-LCG_external_package(pythia8           183            ${MCGENPATH}/pythia8      author=183     )
-LCG_external_package(pythia8           185            ${MCGENPATH}/pythia8      author=185     )
-LCG_external_package(pythia8           186            ${MCGENPATH}/pythia8      author=186     )
-LCG_external_package(pythia8           180            ${MCGENPATH}/pythia8      author=180     )
+LCG_external_package(pythia8           175            ${MCGENPATH}/pythia8      )
+LCG_external_package(pythia8           183            ${MCGENPATH}/pythia8      )
+LCG_external_package(pythia8           185            ${MCGENPATH}/pythia8      )
+LCG_external_package(pythia8           186            ${MCGENPATH}/pythia8      )
+LCG_external_package(pythia8           201            ${MCGENPATH}/pythia8      )
+LCG_external_package(pythia8           180            ${MCGENPATH}/pythia8      )
 
 LCG_external_package(sacrifice         0.9.9          ${MCGENPATH}/sacrifice  pythia8=185 )
 
