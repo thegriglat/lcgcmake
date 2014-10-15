03b12234c2df88e03f7fdae0cf44342b1da24628
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2014-10-15 14:34:50 +0000

    re-add proper ROOT version
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6596 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-70.cmake
+++ cmake/toolchain/heptools-70.cmake
@@ -15,7 +15,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/heptools-common.cmake)
 LCG_AA_project(COOL  COOL_2_9_3)
 LCG_AA_project(CORAL CORAL_2_4_3)
 LCG_AA_project(RELAX RELAX_1_3_0p)
-LCG_AA_project(ROOT  v5-34-00-patches)
+LCG_AA_project(ROOT  5.34.22)
 LCG_AA_project(LCGCMT LCGCMT_${heptools_version})
 
 # Externals
