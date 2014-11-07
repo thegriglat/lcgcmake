b464866023d475b354b8fa3feaf159ddde93dce9
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2014-11-07 18:22:52 +0000

    Use also muons in in zjets analysis
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6676 4525493e-7705-40b1-a816-d608a930855b

--- generators/rivetgenser/GENSER_MC_ZJETS.cc
+++ generators/rivetgenser/GENSER_MC_ZJETS.cc
@@ -32,8 +32,13 @@ namespace Rivet {
 //      ZFinder zfinder(fs, -2.4, 2.4, 25*GeV, PID::ELECTRON, 65*GeV, 115*GeV, 0.3, ZFinder::CLUSTERNODECAY, ZFinder::TRACK);
       ZFinder zfinder(fs, cut, PID::ELECTRON, 65*GeV, 115*GeV, 0.3, ZFinder::CLUSTERNODECAY, ZFinder::TRACK);
       addProjection(zfinder, "ZFinder");
+      ZFinder zfinder1(fs, cut, PID::MUON, 65*GeV, 115*GeV, 0.3, ZFinder::CLUSTERNODECAY, ZFinder::TRACK);
+      addProjection(zfinder1, "ZFinder1");
+
       FastJets jetpro(zfinder.remainingFinalState(), FastJets::ANTIKT, 0.5);
       addProjection(jetpro, "Jets");
+      FastJets jetpro1(zfinder1.remainingFinalState(), FastJets::ANTIKT, 0.5);
+      addProjection(jetpro1, "Jets1");
 
 
       _h_Z_pt = bookHisto1D("Z_pt", logspace(10, 10., 1000.));
@@ -50,12 +55,23 @@ namespace Rivet {
       const double weight = event.weight();
 
       const ZFinder& zfinder = applyProjection<ZFinder>(event, "ZFinder");
-      if (zfinder.bosons().size() != 1) vetoEvent;
-      const FourMomentum& zmom = zfinder.bosons()[0].momentum();
+      const ZFinder& zfinder1 = applyProjection<ZFinder>(event, "ZFinder1");
+      if (zfinder.bosons().size() != 1 && zfinder1.bosons().size() != 1) vetoEvent;
+      double zpt;
+      if (zfinder.bosons().size() == 1) {
+        const FourMomentum& zmom = zfinder.bosons()[0].momentum();
+        zpt = zmom.pt();
+      }
+      if (zfinder1.bosons().size() == 1) {
+        const FourMomentum& zmom = zfinder1.bosons()[0].momentum();
+        zpt = zmom.pt();
+      }
 
-      _h_Z_pt->fill(zmom.pt(), weight);
+      _h_Z_pt->fill(zpt, weight);
 
-      const Jets& jets = applyProjection<FastJets>(event, "Jets").jetsByPt(20*GeV);
+      Jets jets = applyProjection<FastJets>(event, "Jets").jetsByPt(20*GeV);
+      if (zfinder1.bosons().size() == 1)
+        jets = applyProjection<FastJets>(event, "Jets1").jetsByPt(20*GeV);
 
       _h_jet_mult->fill((double)(jets.size()), weight);
       _h_jet_mult_norm->fill((double)(jets.size()), weight);
