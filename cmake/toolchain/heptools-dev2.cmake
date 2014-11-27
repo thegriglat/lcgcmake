e5cd77a65f353c1c951ced5fb23d203674e55bd1
dkonst <dkonst@4525493e-7705-40b1-a816-d608a930855b>
2014-11-27 09:22:18 +0000

    photos 215.5 removed from dev2 and dev4
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6688 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-dev2.cmake
+++ cmake/toolchain/heptools-dev2.cmake
@@ -198,7 +198,6 @@ LCG_external_package(agile             1.4.1          ${MCGENPATH}/agile        
 LCG_external_package(photos++          3.52           ${MCGENPATH}/photos++     )
 LCG_external_package(photos++          3.56           ${MCGENPATH}/photos++     )
 
-LCG_external_package(photos            215.5          ${MCGENPATH}/photos       ) 
 LCG_external_package(photos            215.4          ${MCGENPATH}/photos       ) 
 
 LCG_external_package(evtgen            1.2.0          ${MCGENPATH}/evtgen         tag=R01-02-00 pythia8=175 tauola++=1.1.1a)
