654d2771077ebebfce2ee08a3a1b4828be0a726a
mato <mato@4525493e-7705-40b1-a816-d608a930855b>
2014-12-02 17:40:34 +0000

    Changed the version of ROOT-5
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6705 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-dev2.cmake
+++ cmake/toolchain/heptools-dev2.cmake
@@ -15,7 +15,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/heptools-common.cmake)
 LCG_AA_project(COOL  COOL_2_9_4)
 LCG_AA_project(CORAL CORAL_2_4_4)
 LCG_AA_project(RELAX RELAX_1_3_0p)
-LCG_AA_project(ROOT  5.34.23)
+LCG_AA_project(ROOT  5.34.24)
 LCG_AA_project(LCGCMT LCGCMT_${heptools_version})
 LCG_external_package(Geant4 9.6.p03 CLHEP=2.1.4.1)
 
