7d2727f251a6d2ab68b2cf21131f3fdb256f507e
mato <mato@4525493e-7705-40b1-a816-d608a930855b>
2014-12-09 13:16:25 +0000

    Attempt to fix relocatability of Python
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6724 4525493e-7705-40b1-a816-d608a930855b

--- externals/Python_postinstall.sh
+++ externals/Python_postinstall.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
-#post-install macro to make Python-config relocatable 
+#!/bin/bash
+# post-install macro to make Python-config and _sysconfigdata.py relocatable
+# usage:  postinstall <INSTALL_DIR>
 
 # replace absolute pythonpath by relative one
 location="\#\!\/usr\/bin\/env python"
 
 sed -i.bak "1s/.*/$location/" $1/bin/python-config
 rm $1/bin/python-config.bak
+
+nam=$(basename `readlink -f $1/bin/python`)
+sed -i '1iimport os; installdir=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))' $1/lib/$nam/_sysconfigdata.py
+sed -i "s:'$1:installdir + ':g" $1/lib/$nam/_sysconfigdata.py
+
