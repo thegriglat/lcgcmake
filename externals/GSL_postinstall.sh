087a4fdf96a07885efe5c638471d46758750d674
mato <mato@4525493e-7705-40b1-a816-d608a930855b>
2014-12-11 14:13:49 +0000

    Add postinstall to GSL
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6743 4525493e-7705-40b1-a816-d608a930855b

--- externals/GSL_postinstall.sh
+++ externals/GSL_postinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+-x
 #post-install macro to make GSL relocatable
 
-# 1) replace absolute prefix by relative one in gsl-config
-sed "s:$1:$(dirname $(dirname $0)):" $1/bin/gsl-config
+# 1) remove .la files
+rm -f $1/lib/*.la
+
+# 2) replace absolute prefix by relative one in gsl-config
+sed -i "s:$1:\$(dirname \$(dirname \$0)):" $1/bin/gsl-config
