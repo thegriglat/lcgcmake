0676dfbd4f28d29d49cbabc93a4904646bc0125b
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2014-12-14 12:35:43 +0000

    Fix madgraph5amc tests
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6756 4525493e-7705-40b1-a816-d608a930855b

--- generators/madgraph5amc/tests/modify.sh
+++
@@ -1,7 +0,0 @@
-cd ${1}
-sed "s%# lhapdf = lhapdf-config% lhapdf = ${2}%" < me5_configuration.txt > me5_configuration.txtNEW
-mv me5_configuration.txtNEW me5_configuration.txt
-sed '/= pdlabel/s,^.*$, lhapdf = pdlabel,' < run_card.dat > run_card.datNEW
-mv run_card.datNEW run_card.dat
-sed "/= lhaid/s,^.*$, 10800 = lhaid," < run_card.dat > run_card.datNEW
-mv run_card.datNEW run_card.dat
