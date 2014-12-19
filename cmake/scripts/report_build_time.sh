57502aea1dea2e9211165db753984dd7681a22f5
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2014-12-19 11:04:01 +0000

    Improved build time report
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6811 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/report_build_time.sh
+++ cmake/scripts/report_build_time.sh
@@ -7,9 +7,11 @@ tmpfile=$(mktemp)
 find $tdir -name '*-start.timestamp' | awk -F'-start.timestamp' '{print $1}' | sed "s@$tdir/@@g" | xargs -n 1 -i{} echo {} >> $tmpfile
 
 cat $tmpfile | while read pkg;do
-  secs=$(echo "$(date -r $tdir/$pkg-stop.timestamp -u "+%s") - $(date -r $tdir/$pkg-start.timestamp -u "+%s")" | bc)
-  echo -e "$pkg\t\t: $(python -c "hours=$secs/60**2;minutes=$secs/60-hours*60;secs=$secs - hours*60**2 - minutes*60; print \"{0}:{1}:{2}\".format(hours, minutes, secs)")"
-done | sort
+  if [ -f "$tdir/$pkg-stop.timestamp" -a -f "$tdir/$pkg-start.timestamp" ];then
+    secs=$(echo "$(date -r $tdir/$pkg-stop.timestamp -u "+%s") - $(date -r $tdir/$pkg-start.timestamp -u "+%s")" | bc)
+    python -c "hours=$secs/60**2;minutes=$secs/60-hours*60;secs=$secs - hours*60**2 - minutes*60; print \"{0:30s} : {1:6d} second(s) ({2:2d} hour(s) {3:2d} minute(s) {4:2d} second(s))\".format(\"$pkg\", $secs, hours, minutes, secs)"
+  fi
+done | sort -n -k 3 -r
 
 rm -f $tmpfile
 exit 0
