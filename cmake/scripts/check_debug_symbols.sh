be24c7fd959ab9c1ba6f07c95a4a8905bf250cd2
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2014-12-27 15:41:17 +0000

    Fixes for debug test, added labels for common tests
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6841 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/check_debug_symbols.sh
+++ cmake/scripts/check_debug_symbols.sh
@@ -8,6 +8,7 @@
 #
 
 ROOT_PATH=$1
+build_type=$2
 OS=$(uname)
 if [ "Darwin" == $OS ];then
   chkcmd="mac_check"
@@ -25,13 +26,13 @@ mac_check(){
 
 echo "Checking debug symbols in ${ROOT_PATH}/* ..."
 exitcode=0
-idx=0
-if echo ${ROOT_PATH} | grep -q '\-opt'; then
+
+if [ "opt" = "$build_type" ]; then
     echo "The following libraries have debug symbols:"
     for dirs in $(find $ROOT_PATH -maxdepth 4 -type d); do
+      echo $dirs | grep -q -v $build_type && continue 
       for name in $([ -d "$dirs/lib" ] && find "$dirs/lib" -name '*\.so' -o -name '*\.dylib') \
                   $([ -d "$dirs/bin" ] && find "$dirs/bin" ); do
-        let "idx = $idx + 1"
         if file $name 2>/dev/null | grep -q ELF && [ ! -z "$($chkcmd $name)" ]; then
             if echo "$name" | grep -q -i MCGenerators && echo "$name" | grep -q -v site-packages; then
               exitcode=1
@@ -43,18 +44,17 @@ if echo ${ROOT_PATH} | grep -q '\-opt'; then
         fi
       done
     done | sort
-elif echo ${ROOT_PATH} | grep -q '\-dbg'; then
+elif [ "dbg" = "$build_type" ]; then
     echo "The following libraries don't have debug symbols:"
     for dirs in $(find $ROOT_PATH -maxdepth 4 -type d); do
-      for name in $([ -d "$dirs/lib" ] && find $dirs/lib -name '*\.so' -o -name '*\.dylib' | grep '\-dbg') \
-                  $([ -d "$dirs/bin" ] && find $dirs/bin | grep '\-dbg'); do
-        let "idx = $idx + 1"
+      echo $dirs | grep -q -v $build_type && continue 
+      for name in $([ -d "$dirs/lib" ] && find $dirs/lib -name '*\.so' -o -name '*\.dylib') \
+                  $([ -d "$dirs/bin" ] && find $dirs/bin ); do
         if file $name 2>/dev/null | grep -q ELF && [ -z "$($chkcmd $name)" ] ; then
           echo "WARNING: File $name doesn't contain debug symbols."
         fi
       done
     done | sort
 fi
-echo "$idx files checked"
 echo "Exiting with code $exitcode ..."
 exit $exitcode
