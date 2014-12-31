c912be39f772a8f3c9b69d089b18e000b1d4763e
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2014-12-31 00:48:49 +0000

    Added lhapdf-6.1.5 to 67c toolchain
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6850 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-67c.cmake
+++ cmake/toolchain/heptools-67c.cmake
@@ -171,6 +171,7 @@ LCG_external_package(heputils       1.0.0          ${MCGENPATH}/heputils )
 LCG_external_package(mcutils        1.1.0          ${MCGENPATH}/mcutils )
 
 LCG_external_package(lhapdf           5.9.1          ${MCGENPATH}/lhapdf       )
+LCG_external_package(lhapdf           6.1.5          ${MCGENPATH}/lhapdf       )
 LCG_external_package(lhapdf           6.1.4          ${MCGENPATH}/lhapdf       )
 
 LCG_external_package(lhapdfsets        5.9.1          lhapdfsets   )
