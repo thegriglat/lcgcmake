49c38f66f01842ac96119ddfe6c5a685eda5c197
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2015-01-09 15:56:06 +0000

    add command line parameters to copyRPMS. Allow to copy new RPMS into an area different from the official one
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6873 4525493e-7705-40b1-a816-d608a930855b

--- cdash/copyRPMS.py
+++ cdash/copyRPMS.py
@@ -4,16 +4,22 @@ from os import listdir
 from os.path import isfile, join
 import shutil
 import os
+from optparse import OptionParser
+
+# extract command line parameters
+usage = "usage: %prog localarea remotearea"
+parser = OptionParser(usage)
+(options, args) = parser.parse_args()    
+if len(args) != 1:
+    parser.error("incorrect number of arguments.")
+else:
+    localarea = args[0]   #where RPMS have been build 
+    remotearea =  args[1] #where RPMS should be copied to
 
 # the official area for the RPMS
 officialarea = "/afs/cern.ch/sw/lcg/external/rpms/lcg"
 
-# the place where the RPMs have been created
-tmp_area = os.environ.get("TMP_RPM_AREA")
-if tmp_area in (None, "none") :
-    print "Environment variable TMP_RPM_AREA not specified; skipping copy of RPMS to %s" %officialarea
-    sys.exit(0)
-buildarea = join(os.environ.get("TMP_RPM_AREA"),"rpmbuild/RPMS/noarch/") 
+buildarea = join(localarea,"rpmbuild/RPMS/noarch/") 
 
 existingrpms = set( f.split("-1.0.0")[0] for f in listdir(officialarea) if isfile(join(officialarea,f)) )
 newrpms   = set( f for f in listdir(buildarea) if isfile(join(buildarea,f)) )
@@ -23,5 +29,5 @@ newrpms   = set( f for f in listdir(buildarea) if isfile(join(buildarea,f)) )
 # installing a rebuild is an exceptional task and shouldn't be default 
 for item in newrpms:
   if item.split("-1.0.0")[0] not in existingrpms:
-    shutil.copy2(join(buildarea,item), officialarea)
-    print "Copied %s to %s" %(item, officialarea)
+    shutil.copy2(join(buildarea,item), remotearea)
+    print "Copied %s to %s" %(item, remotearea)
