7982f111d1b8292b0c6bec6e80bd2947e2b5fde3
pmendez <pmendez@4525493e-7705-40b1-a816-d608a930855b>
2015-01-15 14:48:07 +0000

    fix relax version
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6885 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-72a.cmake
+++ cmake/toolchain/heptools-72a.cmake
@@ -14,7 +14,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/heptools-common.cmake)
 # Application Area Projects
 LCG_AA_project(COOL  COOL_2_9_5)
 LCG_AA_project(CORAL CORAL_2_4_5)
-LCG_AA_project(RELAX RELAX_1_4_2)
+LCG_AA_project(RELAX RELAX_1_3_0p)
 LCG_AA_project(ROOT  5.34.25)
 LCG_AA_project(LCGCMT LCGCMT_${heptools_version})
 LCG_AA_project(HepMC 2.06.09)
