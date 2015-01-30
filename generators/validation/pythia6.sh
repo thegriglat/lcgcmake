16ad7d145274ed3b195d44c4975371deda0ece5b
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2015-01-30 11:05:27 +0000

    Fixes for validation targets
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6929 4525493e-7705-40b1-a816-d608a930855b

--- generators/validation/pythia6.sh
+++ generators/validation/pythia6.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-extra_tests="rivet-tests.genser-buildanalysis pythia6-$version.build"
+extra_tests="pythia6-$version.build"
 svn_config_files="generators/agile_configs/pythia6-ttbar-8tev.params"
--- generators/validation/pythia8.sh
+++ generators/validation/pythia8.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-extra_tests="pythia8-$version.build rivet-tests.genser-prepare_references"
+extra_tests="pythia8-$version.build"
 build_config_files="generators/pythia8/tests/pythia8-${version}.zjetsgenser-rivet.params"
