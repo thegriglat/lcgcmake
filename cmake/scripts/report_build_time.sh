8680c2b26ad30b4d37fe83e1d84e88c5a5a832b5
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2015-02-02 06:57:47 +0000

    Fixes for MacOS: report build time test
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6933 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/report_build_time.sh
+++ cmake/scripts/report_build_time.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tdir="$1"
-tmpfile=$(mktemp)
+tmpfile=$(mktemp -t XXXXX)
 
 # get packages names
 find $tdir -name '*-start.timestamp' | awk -F'-start.timestamp' '{print $1}' | sed "s@$tdir/@@g" | xargs -n 1 -i{} echo {} >> $tmpfile
