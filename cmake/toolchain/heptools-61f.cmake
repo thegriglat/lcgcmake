7143764dacf6b518c3ff9877b0ba14015af0c1a0
dkonst <dkonst@4525493e-7705-40b1-a816-d608a930855b>
2015-02-03 19:57:12 +0000

    crmc 1.4 and 1.5.3 added to 61f
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@6943 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-61f.cmake
+++ cmake/toolchain/heptools-61f.cmake
@@ -217,6 +217,9 @@ LCG_external_package(starlight         r43            ${MCGENPATH}/starlight    
 #LCG_external_package(herwig            6.520         ${MCGENPATH}/herwig       )
 LCG_external_package(herwig            6.520.2        ${MCGENPATH}/herwig       )
 LCG_external_package(crmc              v3400          ${MCGENPATH}/crmc         )
+LCG_external_package(crmc              1.4            ${MCGENPATH}/crmc         )
+LCG_external_package(crmc              1.5.3          ${MCGENPATH}/crmc         )
+
 LCG_external_package(cython            0.19           ${MCGENPATH}/cython       )
 LCG_external_package(yamlcpp           0.3.0          ${MCGENPATH}/yaml-cpp     )
 LCG_external_package(yoda              1.0.0          ${MCGENPATH}/yoda         )
