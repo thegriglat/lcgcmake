e82f61a9f6664b338a2578946a3e51445db4395f
hegner <hegner@4525493e-7705-40b1-a816-d608a930855b>
2015-02-16 10:20:43 +0000

    fixing SPI-761
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@7035 4525493e-7705-40b1-a816-d608a930855b

--- pyexternals/Python_postinstall.sh
+++ pyexternals/Python_postinstall.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 #post-install macro to make Python-executables relocatable 
 
 # replace absolute pythonpath by relative one
 location="\#\!\/usr\/bin\/env python"
 
 for line in `find $1/bin -iname '*'`; do
-  echo "Patching $line for relocatability"
-  sed -i.bak -e "1s/.*/$location/" $line
-  rm $line.bak 
+  if head -n1 $line | grep -q "/python" ; then
+    echo "Patching $line for relocatability"
+    sed -i.bak -e "1s/.*/$location/" $line
+    rm $line.bak 
+  fi
 done
 
