2bb6e0ccfa7cdefadeb4f58d1c777c87c5b0f21f
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2015-02-27 18:24:21 +0000

    Fix report_build_time.sh for Darwin
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@7104 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/report_build_time.sh
+++ cmake/scripts/report_build_time.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 tdir="$1"
-tmpfile=$(mktemp -t lcg.XXX)
+tmpfile=$(mktemp -t XXXXX)
+format="-c %Y"
+ARCH="`uname`"
+if [ $ARCH = Darwin ]; then
+  format="-f %m"
+fi
 
 # get packages names
-find $tdir -name '*-start.timestamp' | awk -F'-start.timestamp' '{print $1}' | sed "s@$tdir/@@g" | xargs -n 1 -i{} echo {} >> $tmpfile
+find $tdir -name '*-start.timestamp' | awk -F'-start.timestamp' '{print $1}' | sed "s@$tdir/@@g" | xargs -n 1 -I {} echo {} >> $tmpfile
 
 cat $tmpfile | while read pkg;do
   if [ -f "$tdir/$pkg-stop.timestamp" -a -f "$tdir/$pkg-start.timestamp" ];then
-    secs=$(echo "$(date -r $tdir/$pkg-stop.timestamp -u "+%s") - $(date -r $tdir/$pkg-start.timestamp -u "+%s")" | bc)
+    secs=$(echo "$(stat $format $tdir/$pkg-stop.timestamp) - $(stat $format $tdir/$pkg-start.timestamp)" | bc)
     python -c "hours=$secs/60**2;minutes=$secs/60-hours*60;secs=$secs - hours*60**2 - minutes*60; print \"{0:30s} : {1:6d} second(s) ({2:2d} hour(s) {3:2d} minute(s) {4:2d} second(s))\".format(\"$pkg\", $secs, hours, minutes, secs)"
   fi
 done | sort -n -k 3 -r
