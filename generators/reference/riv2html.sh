9cf1a9f490cc2b6e13263cd1c4e3c4f943878dc0
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2015-03-04 13:17:32 +0000

    Copying of yoda files to AFS made conditional
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@7124 4525493e-7705-40b1-a816-d608a930855b

--- generators/reference/riv2html.sh
+++ generators/reference/riv2html.sh
@@ -11,24 +11,41 @@ echo end arguments
 
 RESULTSTORE=/afs/cern.ch/sw/lcg/external/MCGenerators_test/validation
 
-mkdir -p ${RESULTSTORE}/yoda
-#cp -f ${2}_riv2_${4}.yoda ${RESULTSTORE}/yoda/${2}_${4}_ref.yoda
-#cp -f ${2}_riv2_${4}_i.yoda ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}_${6}.yoda
-# new names:
-cp -f ${2}.genser-${4}-rivet.yoda ${RESULTSTORE}/yoda/${2}_${4}_ref.yoda
-cp -f ${2}-${3}.genser-${4}-rivet.yoda ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}_${6}.yoda
-
-echo
-echo yoda file with results is produced and put together with the reference on AFS
-echo
-echo to get html page with comparison in the directory ./myhtml run the following:
-echo
-echo ${RESULTSTORE}/runmkhtml.sh ${RESULTSTORE}/yoda/${2}_${4}_ref.yoda ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}_${6}.yoda myhtml
-echo
-echo after this you can see the page by typing:
-echo
-echo firefox myhtml/index.html
-echo
+if [[ -w ${RESULTSTORE} ]] ; then
+  mkdir -p ${RESULTSTORE}/yoda
+  cp -f ${2}.genser-${4}-rivet.yoda ${RESULTSTORE}/yoda/${2}_${4}_ref.yoda
+  cp -f ${2}-${3}.genser-${4}-rivet.yoda ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}_${6}.yoda
+  echo
+  echo yoda file with results is produced and put together with the reference on AFS
+  echo
+  echo to get html page with comparison in the directory ./myhtml run the following:
+  echo
+  echo ${RESULTSTORE}/runmkhtml.sh ${RESULTSTORE}/yoda/${2}_${4}_ref.yoda ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}_${6}.yoda myhtml
+  echo
+  echo after this you can see the page by typing:
+  echo
+  echo firefox myhtml/index.html
+  echo
+else
+  echo
+  echo AFS store is not available or not writable
+  echo to get html page with comparison in the directory ./myhtml do the following:
+  echo
+  echo login to ${HOSTNAME} and use scp or sftp for copying below
+  echo
+  mylocation=$(pwd)
+  echo copy ${mylocation}/${2}.genser-${4}-rivet.yoda  to  ${RESULTSTORE}/yoda/${2}_${4}_ref.yoda
+  echo copy ${mylocation}/${2}-${3}.genser-${4}-rivet.yoda  to  ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}_${6}.yoda
+  echo
+  echo then on the node with AFS do the following:
+  echo
+  echo ${RESULTSTORE}/runmkhtml.sh ${RESULTSTORE}/yoda/${2}_${4}_ref.yoda ${RESULTSTORE}/yoda/${2}_${3}_${4}_${5}_${6}.yoda myhtml
+  echo
+  echo after this you can see the page by typing:
+  echo
+  echo firefox myhtml/index.html
+  echo
+fi
 
 #source $1/rivetenv.sh
 #rivet-mkhtml -o ${2}html -s --mc-errs ${2}_riv2_${4}.yoda:'Title=Reference' ${2}_riv2_${4}_i.yoda:'LineColor=blue'
