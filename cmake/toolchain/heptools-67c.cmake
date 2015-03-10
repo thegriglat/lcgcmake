5d64eb9ad2ddda03b13a5e29645c2d61c5bea75c
mkirsano <mkirsano@4525493e-7705-40b1-a816-d608a930855b>
2015-03-10 15:01:24 +0000

    Add powheg-box-v2 and dependencies to 67c
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@7153 4525493e-7705-40b1-a816-d608a930855b

--- cmake/toolchain/heptools-67c.cmake
+++ cmake/toolchain/heptools-67c.cmake
@@ -179,6 +179,10 @@ LCG_external_package(lhapdfsets        5.9.1          lhapdfsets   )
 LCG_external_package(madgraph5amc       2.2.1          ${MCGENPATH}/madgraph5amc )
 
 LCG_external_package(powheg-box         r2092         ${MCGENPATH}/powheg-box   )
+LCG_external_package(powheg-box-v2      r3043.lhcb    ${MCGENPATH}/powheg-box-v2 author=r3043  )
+ 
+LCG_external_package(feynhiggs         2.10.2         ${MCGENPATH}/feynhiggs       )
+LCG_external_package(chaplin           1.2            ${MCGENPATH}/chaplin         )
 
 LCG_external_package(pythia8           175.lhetau     ${MCGENPATH}/pythia8      author=175     )
 LCG_external_package(pythia8           175            ${MCGENPATH}/pythia8      author=175     )
