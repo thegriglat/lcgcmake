70202a78c6dea6cde726eb81e74b4cb7984641bf
mato <mato@4525493e-7705-40b1-a816-d608a930855b>
2015-03-29 16:03:24 +0000

    simplify a bit
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@7213 4525493e-7705-40b1-a816-d608a930855b

--- cdash/cleanbuildmachines.py
+++ cdash/cleanbuildmachines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import sys, shutil, os
-from os import path
+import shutil
+from os import path, listdir, getcwd
 from datetime import datetime, timedelta
 from optparse import OptionParser
 
+#---Globals-----------------------------------------------------------
 mindate = 0
 controlfile = 'controlfile'
 
@@ -13,24 +14,20 @@ def ctime(filename):
 
 def find_dirs_to_clean(cdir, depth):
    global mindate, controlfile
-   check_file = os.path.join(cdir,controlfile)
-   if os.path.exists(check_file):
-      if ctime(check_file) < mindate :
-         return [cdir]
-      else:
-         return []
+   result = []
+   check_file = path.join(cdir,controlfile)
+   if path.exists(check_file) and ctime(check_file) < mindate:
+      result += [cdir]
    elif depth <= 0:
-      return []
+      pass
    else:
-      result = []
-      for d in os.listdir(cdir):
-         fulldir = os.path.join(cdir,d)
-         if os.path.isdir(fulldir):
+      for d in listdir(cdir):
+         fulldir = path.join(cdir,d)
+         if path.isdir(fulldir):
             result += find_dirs_to_clean(fulldir, depth-1)
-      return result
+   return result
 
 if __name__ == "__main__":
-   # extract command line parameters
    usage = "usage: %prog rootdir"
    parser = OptionParser(usage)
    parser.add_option('-a', '--age', type='int',
@@ -41,11 +38,11 @@ if __name__ == "__main__":
                      default=10)
 
    (options, args) = parser.parse_args()
-   if len(args) == 0: rootdir = os.getcwd()
+   if len(args) == 0: rootdir = getcwd()
    else:              rootdir = args[0]
 
    mindate = datetime.now() - timedelta(hours = options.min_age)
    dirs_to_clean = find_dirs_to_clean(rootdir, options.max_depth)
    for d in dirs_to_clean:
-      print 'Removing directory %s, built on %s' %(d,ctime(os.path.join(d,controlfile)))
+      print 'Removing directory %s, built on %s' %(d,ctime(path.join(d,controlfile)))
       shutil.rmtree(d)
