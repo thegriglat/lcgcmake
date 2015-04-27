9b46081b3eea2c13d0991e57d352e31483ef7257
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2015-04-27 06:45:27 +0000

    Minor changes in check_toolchains.sh
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@7330 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/check_toolchains.sh
+++ cmake/scripts/check_toolchains.sh
@@ -2,23 +2,31 @@
 
 source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6-gcc49-opt/setup.sh
 export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/2.8.12.2/Linux-i386/bin:${PATH}
+export CC=gcc
+export CXX=g++
+export FC=gfortran
 
-WORKDIR=/tmp/${USER}/testlcg
+if [ -z "$1" ]; then
+  LIST="$(ls lcgcmake/cmake/toolchain/*.cmake | cut -d- -f2 | cut -d. -f 1 | grep -v common | sort)"
+else
+  LIST="$1"
+fi
 
-mkdir -p ${WORKDIR}
+WORKDIR="$(mktemp -t -d check_toolchain.XXXX)"
+
+echo "WORKDIR = $WORKDIR"
 cd ${WORKDIR}
 rm -rf lcgcmake build_* out_lcgconfigure*
 
 svn co svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake
 
-for TOOLCHAIN in dev2 dev3 dev4 experimental 67b 67c 61f 64d 71 71root6 73root6 74root6 75root6 geantv rootext
+for TOOLCHAIN in $LIST
 do
     echo -n " Checking toolchain ${TOOLCHAIN}..... "
     mkdir ${WORKDIR}/build_${TOOLCHAIN}
     cd ${WORKDIR}/build_${TOOLCHAIN}
-cmake -DLCG_VERSION=${TOOLCHAIN} -DCMAKE_INSTALL_PREFIX=../install -DPDFsets=minimal -DLCG_SAFE_INSTALL=ON -DLCG_TARBALL_INSTALL=false -DCMAKE_BUILD_TYPE:STRING=Release ../lcgcmake >& ../out_lcgconfigure_${TOOLCHAIN}
-    status=$?
-    if [ $status -ne 0 ]; then
+cmake -DLCG_VERSION=${TOOLCHAIN} -DCMAKE_INSTALL_PREFIX=../install -DPDFsets=minimal -DLCG_SAFE_INSTALL=ON -DLCG_TARBALL_INSTALL=OFF -DCMAKE_BUILD_TYPE:STRING=Release ../lcgcmake >& ../out_lcgconfigure_${TOOLCHAIN}
+    if [ $? -ne 0 ]; then
         echo "error, output can be found here: ${WORKDIR}/out_lcgconfigure_${TOOLCHAIN}"
     else
         echo "OK!"	
