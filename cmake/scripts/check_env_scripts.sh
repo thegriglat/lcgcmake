a1832eeafcf57e54bd5edc37f6f67d13fa2226a4
glatyshe <glatyshe@4525493e-7705-40b1-a816-d608a930855b>
2015-05-18 08:55:24 +0000

    Repair generators checking (for prev. commit)
    git-svn-id: svn+ssh://svn.cern.ch/reps/lcgsoft/trunk/lcgcmake@7408 4525493e-7705-40b1-a816-d608a930855b

--- cmake/scripts/check_env_scripts.sh
+++ cmake/scripts/check_env_scripts.sh
@@ -2,7 +2,7 @@
 
 tmp="mktemp"
 
-find "$1" -type f -name '*-env.sh' -o -name '-*env-genser.sh' | while read i; do
+find "$1" -type f -name '*-env.sh' -o -name '*env-genser.sh' | while read i; do
   echo -n "Checking $i ... "
   if source $i | grep -qi cannot ;then
     echo "FAILED. Cannot properly source $i:"
